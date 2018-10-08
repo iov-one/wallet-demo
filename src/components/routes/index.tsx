091cb80f79ffe02e573fa0fd0536566a82e80d84
Daniel Zheng <dannyzheng1993@gmail.com>
2018-10-09 01:25:28 +0800

    Removed Open-browser-webpack-plugin Changes for the last comment.

--- src/components/routes/index.tsx
+++ src/components/routes/index.tsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as HashRouter, Route } from "react-router-dom";
 
 import { CounterContainer, Home } from "../pages";
 
 const MainRouter = () => (
-  <Router>
+  <HashRouter>
     <div>
       <Route exact path="/" component={Home} />
       <Route path="/counter/" component={CounterContainer} />
     </div>
-  </Router>
+  </HashRouter>
 );
 
 export default MainRouter;
