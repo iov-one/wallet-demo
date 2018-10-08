d3cc09879c61e36ab1601442de0b61bf723fdfd8
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-10-08 12:53:17 +0200

    Add webpack with hotreloading, manually open page

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+class App extends React.Component<{}, {}> {
+    public render(): JSX.Element {
+        return (
+            <div>
+                <h1>Hello, World!</h1>
+                <p>Hope you're having a great day</p>
+            </div>
+        );
+    }
+}
+
+export default App;
