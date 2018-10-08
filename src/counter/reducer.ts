356c32d495043a214e9320950fd691e46b6ec1e6
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-10-08 13:26:40 +0200

    Configure basic redux store

--- src/counter/reducer.ts
+++ src/counter/reducer.ts
@@ -3,10 +3,10 @@ import { ActionType } from "typesafe-actions";
 import * as actions from "./actions";
 import { CounterState } from "./state";
 
-export type CounterAction = ActionType<typeof actions>;
+export type CounterActions = ActionType<typeof actions>;
 const initState: CounterState = 1;
 
-export function counterReducer(state: CounterState = initState, action: CounterAction): CounterState {
+export function counterReducer(state: CounterState = initState, action: CounterActions): CounterState {
   switch (action.type) {
     // separate line for each success
     case "INCREMENT_COUNTER":
---
+++ src/store.ts
@@ -0,0 +1,25 @@
+// import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
+import { applyMiddleware, combineReducers, compose, createStore, Middleware } from "redux";
+import promiseMiddleware from "redux-promise-middleware";
+import thunk from "redux-thunk";
+
+import { CounterActions, counterReducer, CounterState } from "./counter";
+
+const middlewares: ReadonlyArray<Middleware> = [/* scopeTie, */ thunk, promiseMiddleware()];
+
+// const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = compose;
+
+export interface RootState {
+  readonly counter: CounterState;
+}
+
+export type RootActions = CounterActions;
+
+const rootReducer = combineReducers<RootState, RootActions>({
+  counter: counterReducer,
+});
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
+
+export { store };
