b438f09851a6b61b176b188cead235c1adf70545
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-10-10 21:01:41 +0200

    Add basic bns connection promise and test

---
+++ src/logic/connection.spec.ts
@@ -0,0 +1,25 @@
+// tslint:disable:no-unused-expression
+import { expect } from 'chai';
+import 'mocha';
+
+import { IovWriter } from '@iov/core';
+
+import { addBlockchain, BlockchainSpec, CodecType } from "./connection";
+import { createProfile } from "./profile";
+
+const testSpec: BlockchainSpec = {
+    codecType: CodecType.Bns,
+    bootstrapNodes: ["wss://localhost:23456"],
+}
+
+describe("addBlockchain", () => {
+    it("should connect to local testnet", async () => {
+        const profile = await createProfile();
+        const writer = new IovWriter(profile);
+        const reader = await addBlockchain(writer, testSpec);
+        expect(reader).to.be.ok;
+        // basic checks that we connected properly
+        expect(reader.chainId).to.include("chain-");
+        expect(await reader.height()).to.be.greaterThan(1);
+    });
+});
---
+++ src/logic/connection.ts
@@ -0,0 +1,51 @@
+import { BcpConnection, ChainConnector } from "@iov/bcp-types";
+import { bnsConnector } from "@iov/bns";
+import { ChainId, IovWriter } from "@iov/core";
+// import { UserProfile } from "@iov/keycontrol";
+
+/**** this may make it into iov-core *******/
+
+export enum CodecType {
+    Bns = "bns",
+    // Lisk = "lisk", 
+}
+
+// BlockchainSpec is a config option, such as may be returned from bns in the future
+export interface BlockchainSpec {
+    readonly chainId?: ChainId;  // if non-empty, enforce this
+    readonly codecType: CodecType;
+    readonly bootstrapNodes: ReadonlyArray<string>;
+}
+
+function specToConnector(spec: BlockchainSpec): ChainConnector {
+    if (spec.bootstrapNodes.length < 1) {
+        throw new Error("No bootstrap nodes defined");
+    }
+    // TODO: select them round-robin
+    const uri = spec.bootstrapNodes[0];
+
+    switch (spec.codecType) {
+        case CodecType.Bns:
+        return bnsConnector(uri);
+        default:
+        throw new Error(`Unsupported codecType: ${spec.codecType}`);
+    }
+}
+
+/******* end iov-core proposal ********/
+
+export async function addBlockchain(writer: IovWriter, blockchain: BlockchainSpec): Promise<BcpConnection> {
+    const connector = specToConnector(blockchain);
+
+    // TODO: return the new chainId in promise....
+    await writer.addChain(connector);
+    const chainIds = writer.chainIds();
+    const chainId = chainIds[chainIds.length-1];
+
+    // TODO: enforce this in writer.addChain?
+    if (blockchain.chainId !== undefined && chainId !== blockchain.chainId) {
+        throw new Error(`Expected chain ${blockchain.chainId}, got ${chainId}`);
+    }
+
+    return writer.reader(chainId);
+}
