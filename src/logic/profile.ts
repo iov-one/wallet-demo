11a82d97b6896cc6da75d2a323af9e82c33a924e
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-10-10 15:37:58 +0200

    Expose full load/save profile to disk functionality

--- src/logic/profile.ts
+++ src/logic/profile.ts
@@ -1,6 +1,8 @@
 import { Bip39, Random } from "@iov/crypto";
 import { Ed25519HdWallet, HdPaths, LocalIdentity, UserProfile } from "@iov/keycontrol";
 
+import { hasDbKey, StringDB } from "./db";
+
 const EntropyBytes = 16;
 
 // initializes a UserProfile with one keyring and one identity using random seed
@@ -27,4 +29,23 @@ export function getMainIdentity(profile: UserProfile): LocalIdentity {
         throw new Error("There must be an identity on the first wallet");
     }
     return idents[0];
+}
+
+// returns true if there is a profile to load
+export async function hasStoredProfile(db: StringDB): Promise<boolean> {
+    // copied from userProfile.ts.... there should be a cleaner way
+    const storageKeyCreatedAt = "created_at";
+    const storageKeyKeyring = "keyring";
+    return (await hasDbKey(db, storageKeyCreatedAt)) && hasDbKey(db, storageKeyKeyring);
+}
+
+// loads the profile if possible, otherwise creates a new one and saves it
+// throws an error on existing profile, but bad password
+export async function loadOrCreateProfile(db: StringDB, password: string): Promise<UserProfile> {
+    if (await hasStoredProfile(db)) {
+        return UserProfile.loadFrom(db, password);
+    }
+    const profile = await createProfile();
+    await profile.storeIn(db, password);
+    return profile;
 }
