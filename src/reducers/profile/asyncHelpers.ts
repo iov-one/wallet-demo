55ebf2620a02ead49658885166883e13a7aec606
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-10-14 16:52:47 +0200

    Define our async helpers

--- src/reducers/profile/asyncHelpers.ts
+++ src/reducers/profile/asyncHelpers.ts
@@ -1,23 +1,24 @@
 import { UserProfile } from "@iov/core";
 
-import { createAsyncAction } from "typesafe-actions";
+import { ActionType, createAsyncAction } from "typesafe-actions";
 
 import { loadOrCreateProfile } from "../../logic";
 
 import { Fn } from "./functions";
 import { funcToAction } from "./syncHelpers";
 
-export type PromiseFn<P, A1, A2, A3> = Fn<Promise<P>, A1, A2, A3>;
 
 // tslint:disable-next-line:no-empty
 function voidFunc(): void {}
 
-export const createActionFromPromise = <T1 extends string, T2 extends string, T3 extends string>(
-  //   req: T0,
+export type PromiseFn<P, A1, A2, A3> = Fn<Promise<P>, A1, A2, A3>;
+export const createActionFromPromise = <T0 extends string, T1 extends string, T2 extends string, T3 extends string>(
+  send : T0,
   pend: T1,
   suc: T2,
   err: T3,
-) => <P, A1, A2, A3>(_: PromiseFn<P, A1, A2, A3>) => ({
+) => <P, A1, A2, A3>(fn: PromiseFn<P, A1, A2, A3>) => ({
+  sending: funcToAction(send, fn),
   request: funcToAction(pend, voidFunc),
   success: funcToAction(suc, (p: P) => p),
   failure: funcToAction(err, (e: Error) => e),
@@ -29,11 +30,30 @@ export const profileAsyncAction = createAsyncAction(
   "CREATE_PROFILE_SUCCESS",
   "CREATE_PROFILE_REJECTED",
 )<void, UserProfile, Error>();
-type ExpectedType = typeof profileAsyncAction;
+// type ExpectedType = typeof profileAsyncAction;
 
 // this is how we handle return values
-export const profilePromiseAction: ExpectedType = createActionFromPromise(
+export const profilePromiseAction = createActionFromPromise(
+  "CREATE_PROFILE",
   "CREATE_PROFILE_PENDING",
   "CREATE_PROFILE_SUCCESS",
   "CREATE_PROFILE_REJECTED",
 )(loadOrCreateProfile);
+
+// trial with typescript
+// type Action = ActionType<typeof profilePromiseAction>;
+// export function foo(act: Action): void {
+//     switch(act.type) {
+//         case "CREATE_PROFILE_SUCCESS":
+//         // act.payload.addEntry();
+//         console.log("YES");
+//         break;
+//         case "CREATE_PROFILE":
+//         // act.payload.then(x => 0);
+//         console.log("YES");
+//         break;
+//         default:
+//         console.log(`What? ${act.type}`);
+//     }
+// };
+
