efeb05977617254285a9ecb389fb64540533332e
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-10-15 13:53:46 +0200

    Remove counter app

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -2,16 +2,14 @@ import { combineReducers } from "redux";
 import { StateType } from "typesafe-actions";
 
 import { BlockchainActions, blockchainReducer } from "./blockchain";
-import { CounterActions, counterReducer } from "./counter";
 import { ProfileActions, profileReducer } from "./profile";
 
 const rootReducer = combineReducers({
   blockchain: blockchainReducer,
-  counter: counterReducer,
   profile: profileReducer,
 });
 
-export type RootActions = BlockchainActions | CounterActions | ProfileActions;
+export type RootActions = BlockchainActions | ProfileActions;
 export type RootState = StateType<typeof rootReducer>;
 
 export default rootReducer;
