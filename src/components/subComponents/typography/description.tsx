0421f11bdfef46b907ec85f8e07aebc5a487c9ff
Daniel Zheng <dannyzheng1993@gmail.com>
2018-10-15 17:19:26 +0800

    Change ignore case file naming to false.

---
+++ src/components/subComponents/typography/description.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Description = styled.div`
+  font-family: Open Sans;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: normal;
+  font-stretch: normal;
+  line-height: 1.6;
+  letter-spacing: 0.8px;
+  color: #6f749a;
+  margin-bottom: 30px;
+`;
