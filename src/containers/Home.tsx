77dc1eb64779e160955a154736cfdfadd63ed457
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-10-19 17:05:06 +0200

    Improve types for Home

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,10 +1,10 @@
 // tslint:disable:no-empty
 // TODO: remove above comment when the empty onClick is gone
 
-import { BcpConnection } from "@iov/bcp-types";
+// import { BcpConnection } from "@iov/bcp-types";
 import { ChainId, MultiChainSigner, UserProfile } from "@iov/core";
-import { PublicIdentity } from "@iov/keycontrol";
-import { isEmpty } from "lodash";
+// import { PublicIdentity } from "@iov/keycontrol";
+// import { isEmpty } from "lodash";
 import * as React from "react";
 import { connect } from "react-redux";
 
@@ -12,8 +12,8 @@ import { PageStructure } from "../components/compoundComponents/page";
 import { CreateWalletForm } from "../components/templates/forms";
 
 import { BlockchainSpec, CodecType } from "../../logic/connection";
+import { getProfile, getSigner } from "../../selectors";
 import { bootSequence, drinkFaucetSequence, setNameSequence } from "../../sequences";
-import {} from "../../selectors";
 
 const chainSpec: BlockchainSpec = {
   codecType: CodecType.Bns,
@@ -27,7 +27,18 @@ interface HomeState {
   readonly chainId: ChainId;
 }
 
-class Home extends React.Component<any, HomeState> {
+interface HomeProps {
+  readonly profile: UserProfile | undefined;
+  readonly signer: MultiChainSigner | undefined;
+  readonly bootSequence: (
+    password: string,
+    blockchains: ReadonlyArray<BlockchainSpec>,
+  ) => Promise<MultiChainSigner>;
+  readonly drinkFaucetSequence: (facuetUri: string, chainId: ChainId) => Promise<any>;
+  readonly setNameSequence: (name: string, chainId: ChainId) => Promise<any>;
+}
+
+class Home extends React.Component<HomeProps, HomeState> {
   constructor(props: any) {
     super(props);
     this.state = {
@@ -38,7 +49,10 @@ class Home extends React.Component<any, HomeState> {
     };
   }
   public componentDidMount(): any {
-    this.props.bootSequence('test-pass', [chainSpec]);
+    this.props.bootSequence("test-pass", [chainSpec]);
+  }
+  public createAccount(): void {
+    console.log("do this later");
   }
   // public getSnapshotBeforeUpdate(prevProps: any): any {
   //   if (!isEmpty(this.props.profile.internal.profile) && isEmpty(prevProps.profile.internal.profile)) {
@@ -71,13 +85,15 @@ class Home extends React.Component<any, HomeState> {
 }
 
 const mapStateToProps = (state: any): any => ({
-  profile: state.profile,
-  blockchain: state.blockchain,
+  profile: getProfile(state),
+  signer: getSigner(state),
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
-  bootSequence: (password: string, blockchains: ReadonlyArray<BlockchainSpec>) => dispatch(bootSequence(password, blockchains)),
-  drinkFaucetSequence: (facuetUri: string, chainId: ChainId) => dispatch(drinkFaucetSequence(facuetUri, chainId)),
+  bootSequence: (password: string, blockchains: ReadonlyArray<BlockchainSpec>) =>
+    dispatch(bootSequence(password, blockchains)),
+  drinkFaucetSequence: (facuetUri: string, chainId: ChainId) =>
+    dispatch(drinkFaucetSequence(facuetUri, chainId)),
   setNameSequence: (name: string, chainId: ChainId) => dispatch(setNameSequence(name, chainId)),
 });
 
