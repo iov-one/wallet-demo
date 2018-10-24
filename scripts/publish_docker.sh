d2a36376c08127342f32fdc4bc75cd36622dbb2d
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-10-24 15:56:35 +0200

    Fix publish_docker.sh script

--- scripts/publish_docker.sh
+++ scripts/publish_docker.sh
@@ -9,13 +9,21 @@
 # It also assumes to run in the root directory of the project
 
 BUILD_VERSION=$(echo ${TRAVIS_COMMIT} | cut -c 1-10);
-TAG=${TRAVIS_TAG:-latest};
 
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   docker build --pull -t "iov1/wallet-demo:${BUILD_VERSION}" .
   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
-  docker tag  "iov1/wallet-demo:${BUILD_VERSION}" "iov1/wallet-demo:${TAG}";
-  docker push "iov1/wallet-demo:${TAG}";
+
+  # always push latest from a successful master build
+  docker tag  "iov1/wallet-demo:${BUILD_VERSION}" "iov1/wallet-demo:latest";
+  docker push "iov1/wallet-demo:latest";
+
+  # also push a tag (eg. v0.1.0) if there is a git tag for this
+  if [[ "$TRAVIS_TAG" != "" ]]; then
+    docker tag  "iov1/wallet-demo:${BUILD_VERSION}" "iov1/wallet-demo:${TRAVIS_TAG}";
+    docker push "iov1/wallet-demo:${TRAVIS_TAG}";
+  fi
+
   docker logout;
 else
   echo "Only build docker images on master: ${BUILD_VERSION} / ${TAG}";
