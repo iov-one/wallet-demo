b99009bdde2ec1b5cd7fc67b08ad12b2265e1783
Daniel Zheng <dannyzheng1993@gmail.com>
2018-10-27 00:55:33 +0800

    Webpack config udpate

--- webpack/development.config.js
+++ webpack/development.config.js
@@ -12,6 +12,9 @@ module.exports = merge(common, {
     "webpack/hot/only-dev-server",
     // bundle the client for hot reloading
     // only- means to only hot reload for successful updates
+    "webpack-dev-server/client?http://localhost:8080",
+    // bundle the client for webpack-dev-server
+    // and connect to the provided endpoint
   ],
   devtool: "inline-source-map",
   output: {
@@ -22,6 +25,7 @@ module.exports = merge(common, {
     // necessary for HMR to know where to load the hot update chunks
   },
   plugins: [
+    new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       "process.env": {
