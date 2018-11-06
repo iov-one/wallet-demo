4591e10bb44ab04268f00b00ecf22d5a09600c73
Daniel Zheng <dannyzheng1993@gmail.com>
2018-11-06 21:08:28 +0800

    Add Text Field type 2

--- src/components/subComponents/input/index.ts
+++ src/components/subComponents/input/index.ts
@@ -1,4 +1,5 @@
 export * from "./input";
+export * from "./secondaryInput";
 
 //Below will be replaced with new ones in this sprint
 export * from "./textInput";
--- stories/input.stories.js
+++ stories/input.stories.js
@@ -3,6 +3,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 
 import { FormInput } from "../src/components/compoundComponents/form";
+import { SecondaryInput } from "../src/components/subComponents/input";
 
 storiesOf("Input", module)
   .add("Empty Input", () => <FormInput title="Empty" placeholder="IOV or wallet address" />)
@@ -24,4 +25,7 @@ storiesOf("Input", module)
       value="mazie_nitzsche@rebekah.ca"
       notification="Address copied!"
     />
-  ));
+  ))
+  .add("Text field 2: active", () => <SecondaryInput placeholder="Add a note" />)
+  .add("Text field 2: filled", () => <SecondaryInput value="For the rent" />)
+  .add("Text field 2: disabled", () => <SecondaryInput value="No note" />);
