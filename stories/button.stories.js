47974440d74d4376fe42c030f8a0b7ad44745ab1
Daniel Zheng <dannyzheng1993@gmail.com>
2018-11-06 17:30:15 +0800

    Transaction Buttons

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -3,20 +3,21 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
-import { Button, SendReceiveButton } from "../src/components/subComponents/buttons";
+import { Button } from "../src/components/subComponents/buttons";
+import { TransactionButtonGroup } from "../src/components/compoundComponents/sections";
 
 storiesOf("Buttons", module)
   .add("NextButton", () => <Button type="primary" title="Continue" onClick={action("clicked")} icon="next" />)
   .add("Primary Button", () => <Button type="primary" title="Done" loading onClick={action("clicked")} />)
   .add("Cancel Button", () => <Button type="revert" title="Cancel" />)
   .add("Check Status Button", () => <Button type="primary" checked title="Copied" icon="check" />)
-  .add("Send and Receive Button", () => (
-    <SendReceiveButton
-      onSend={() => {
-        action("Send Clicked");
-      }}
+  .add("Transaction Buttons", () => (
+    <TransactionButtonGroup
       onReceive={() => {
-        action("Receive Clicked");
+        action("onReceive");
+      }}
+      onSend={() => {
+        action("onSend");
       }}
     />
   ));
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,6 +2,9 @@ import WebFont from "webfontloader";
 
 WebFont.load({
   google: {
-    families: ["Open Sans:300,400,700"],
+    families: [
+      "Open Sans:300,400,500,600,700",
+      "Muli:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i",
+    ],
   },
 });
