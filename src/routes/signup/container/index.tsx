4fd6d9fb50150693acbaa9056cb907e9312e7f81
apanizo <adolfo.panizo>
2018-11-21 15:30:51 +0100

    Typo importing PageStructure component

--- src/routes/signup/container/index.tsx
+++ src/routes/signup/container/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { PageStructure } from '~/components/compoundComponents/page';
+import { PageStructure } from '~/components/templates/page';
 
 type Props = {
 
