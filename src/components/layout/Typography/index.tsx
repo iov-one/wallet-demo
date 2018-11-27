df2abb712cdcde2542e449b101b53eb0276f5381
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-11-27 08:52:53 +0100

    Adding underlined and inline styles to Typography component

--- src/components/layout/Typography/index.tsx
+++ src/components/layout/Typography/index.tsx
@@ -1,18 +1,19 @@
 import Typography, { TypographyProps } from "@material-ui/core/Typography";
-import classNames from "classnames";
+import classNames from "classnames/bind";
 import * as React from "react";
 import styles from "./index.scss";
 
-const cx = classNames.bind(styles);
+const cx: any = classNames.bind(styles);
 
 interface Props extends TypographyProps {
   readonly underlined?: boolean;
+  readonly inline?: boolean;
 }
 
-const IovTypography = ({ underlined, ...rest }: Props) => {
-  const typographyClassnames = cx({ underlined });
+const IovTypography = ({ underlined, inline, ...rest }: Props) => {
+  const typographyClassnames = cx({ underlined, inline });
 
-  return <Typography className={typographyClassnames} {...rest} />;
+  return <Typography {...rest} className={typographyClassnames} />;
 };
 
 export default IovTypography;
