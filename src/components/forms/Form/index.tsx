964a1de9353e65423c14fa7b71eba1b4c55261e6
apanizo <adolfo.panizo>
2018-11-28 10:49:45 +0100

    Making Form component display block compliant via flex css

--- src/components/forms/Form/index.tsx
+++ src/components/forms/Form/index.tsx
@@ -1,27 +1,57 @@
+import { createStyles, withStyles, WithStyles } from "@material-ui/core";
+import classNames from "classnames";
 import { FormApi, FormState } from "final-form";
 import * as React from "react";
 import { Form } from "react-final-form";
 
-interface Props {
-  readonly onSubmit: (values: object, form: FormApi, callback?: (errors?: object) => void) => object | Promise<object | undefined> | undefined | void;
+const styles = createStyles({
+  form: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  grow: {
+    flexGrow: 1,
+  },
+});
+
+interface Props extends WithStyles<typeof styles> {
+  readonly onSubmit: (
+    values: object,
+    form: FormApi,
+    callback?: (errors?: object) => void,
+  ) => object | Promise<object | undefined> | undefined | void;
   readonly children: (restProps: FormState) => JSX.Element;
   readonly validation?: (values: object) => object | Promise<object>;
   readonly initialValues?: object;
   readonly className?: string;
+  readonly grow?: boolean;
 }
 
-const IovForm = ({ onSubmit, validation, initialValues, children, className, ...props }: Props) => (
-  <Form
-    validate={validation}
-    onSubmit={onSubmit}
-    initialValues={initialValues}
-    render={({ handleSubmit, ...rest }) => (
-      <form className={className} onSubmit={handleSubmit}>
-        {children(rest)}
-      </form>
-    )}
-    {...props}
-  />
-);
+const IovForm = ({
+  onSubmit,
+  classes,
+  grow,
+  validation,
+  initialValues,
+  children,
+  className,
+  ...props
+}: Props) => {
+  const fromClasses = classNames(classes.form, grow ? classes.grow : undefined, className);
 
-export default IovForm;
+  return (
+    <Form
+      validate={validation}
+      onSubmit={onSubmit}
+      initialValues={initialValues}
+      render={({ handleSubmit, ...rest }) => (
+        <form className={fromClasses} onSubmit={handleSubmit}>
+          {children(rest)}
+        </form>
+      )}
+      {...props}
+    />
+  );
+};
+
+export default withStyles(styles)(IovForm);
