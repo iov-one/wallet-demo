c6717e7f08057c466880661d7173096dda60c91d
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-12-01 23:58:07 +0100

    Simplifying Field

--- src/components/forms/Field/index.tsx
+++ src/components/forms/Field/index.tsx
@@ -1,9 +1,3 @@
-import * as React from "react";
-import { Field, FieldProps } from "react-final-form";
+import { Field } from "react-final-form";
 
-interface Props extends FieldProps {
-  readonly component: any;
-}
-const IovField = ({ component, ...props }: Props): JSX.Element => <Field component={component} {...props} />;
-
-export default IovField;
+export default Field;
