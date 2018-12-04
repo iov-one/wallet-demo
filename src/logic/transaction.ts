53a1ede275fe3ab9472ed7520f1440325de83ccd
Ethan Frey <ethanfrey@users.noreply.github.com>
2018-12-04 11:02:03 +0100

    Clean up the transaction watch/parse code

--- src/logic/transaction.ts
+++ src/logic/transaction.ts
@@ -1,33 +1,60 @@
-import { Amount, BcpAccount, BcpConnection, ConfirmedTransaction, SendTx } from "@iov/bcp-types";
+import { PublicKeyBundle } from "@iov/base-types";
+import {
+  BcpConnection,
+  ConfirmedTransaction,
+  SendTx,
+  TransactionKind,
+  UnsignedTransaction,
+} from "@iov/bcp-types";
+import { BnsConnection } from "@iov/bns";
 import { PublicIdentity } from "@iov/keycontrol";
+import { ReadonlyDate } from "readonly-date";
 
-import { getAccount, getAccountByAddress, keyToAddress } from "./account";
+// import { getAccount, getAccountByAddress, keyToAddress } from "./account";
 
-export interface TransNotificationInfo {
+export interface TransNotificationInfo<T extends UnsignedTransaction = SendTx>
+  extends ConfirmedTransaction<T> {
   readonly received: boolean;
-  readonly signerAccount?: BcpAccount;
-  readonly recipientAccount?: BcpAccount;
-  readonly amount: Amount;
-  readonly time?: string;
+  readonly time: ReadonlyDate;
   readonly success: boolean;
+  // these are set for reverse lookup of valuename
+  readonly signerName?: string;
+  readonly recipientName?: string;
 }
+
+const keysEqual = (a: PublicKeyBundle, b: PublicKeyBundle): boolean =>
+  a.algo === b.algo && arraysEqual(a.data, b.data);
+
+const arraysEqual = (a: Uint8Array, b: Uint8Array): boolean => {
+  if (a.length !== b.length) {
+    return false;
+  }
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+  return true;
+};
 
 export const parseConfirmedTransaction = async (
   conn: BcpConnection,
   trans: ConfirmedTransaction,
   identity: PublicIdentity,
 ): Promise<TransNotificationInfo> => {
-  const transaction = trans.transaction as SendTx;
-  const { recipient, amount, signer } = transaction;
-  const recipientAccount = await getAccountByAddress(conn, recipient);
-  const signerAccount = await getAccount(conn, { pubkey: signer });
-  const address = keyToAddress(identity);
-  const received = (recipientAccount && address === recipientAccount.address) || false;
+  const payload = trans.transaction;
+  if (payload.kind !== TransactionKind.Send) {
+    throw new Error(`Only handle SendTx for now, got ${payload.kind}`);
+  }
+  const received = keysEqual(trans.primarySignature.pubkey, identity.pubkey);
+  // TODO: fix this, we cannot always assume BnsConnection
+  const header = await (conn as BnsConnection).getHeader(trans.height);
+  const time = header.time;
+  // TODO: lookup value names
   return {
-    amount,
-    recipientAccount,
-    signerAccount,
+    ...(trans as ConfirmedTransaction<SendTx>),
     received,
+    time,
     success: true,
   };
 };
--- src/reducers/notification/actions.ts
+++ src/reducers/notification/actions.ts
@@ -1,11 +1,6 @@
-import { BcpConnection, ConfirmedTransaction, TxCodec } from "@iov/bcp-types";
-
-import { PendingNotificationItemProps } from "./state";
-
+import { TransNotificationInfo, watchTransaction } from "../../logic";
 import { createSyncAction } from "../helpers";
-
-import { PublicIdentity } from "@iov/keycontrol";
-import { TransNotificationInfo, Unsubscriber, watchTransaction } from "../../logic";
+import { PendingNotificationItemProps } from "./state";
 
 export const addPendingTransactionAction = createSyncAction(
   "ADD_PENDING_TRANSACTION",
@@ -26,13 +21,4 @@ export const pendingTransactionVisited = createSyncAction("PENDING_TRANSACTION_V
 
 export const setTransactionErrorAction = createSyncAction("SET_TRANSACTION_ERROR", (error: string) => error);
 
-function watchTransactionFunction(
-  connection: BcpConnection,
-  ident: PublicIdentity,
-  cb: (transaction?: ConfirmedTransaction, err?: any) => any,
-  codec?: TxCodec,
-): Unsubscriber {
-  return watchTransaction(connection, ident, cb, codec);
-}
-
-export const watchTransactionAction = createSyncAction("WATCH_TRANSACTION", watchTransactionFunction);
+export const watchTransactionAction = createSyncAction("WATCH_TRANSACTION", watchTransaction);
