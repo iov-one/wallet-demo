c928f759a5290364669304b06b75963245d73424
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-12-08 10:23:31 +0100

    One Block per line for fulfill grid approach

--- src/components/pages/PageColumn/index.tsx
+++ src/components/pages/PageColumn/index.tsx
@@ -63,12 +63,10 @@ const Layout = ({
           <React.Fragment>
             <Block scroll grow>
               <Block offsetSm={2}>
-                <GridItem variant="column">
-                  {renderHeader ? renderHeader() : <EmptyHeader />}
-                  <TitleSection primaryTitle={primaryTitle} secondaryTitle={secondaryTitle} />
-                  <SubtitleSection text={subtitle} />
-                  {formRender()}
-                </GridItem>
+                {renderHeader ? renderHeader() : <EmptyHeader />}
+                <TitleSection primaryTitle={primaryTitle} secondaryTitle={secondaryTitle} />
+                <SubtitleSection text={subtitle} />
+                {formRender()}
               </Block>
             </Block>
             <Hairline />
