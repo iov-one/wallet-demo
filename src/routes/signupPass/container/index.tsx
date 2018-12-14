05addb0e7ede3e5ad0d92fb04306dd42f2f21a3a
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-12-14 16:58:08 +0100

    Unifying code for login and create account

--- src/routes/signupPass/container/index.tsx
+++ src/routes/signupPass/container/index.tsx
@@ -1,12 +1,11 @@
-// tslint:disable:no-string-literal
-import config from "config";
 import * as React from "react";
 import { connect } from "react-redux";
 import { FormType } from "~/components/forms/Form";
 import { hasStoredProfile } from "~/logic";
-import { BALANCE_ROUTE, LOGIN_ROUTE, SET_NAME_ROUTE } from "~/routes";
+import { BALANCE_ROUTE, LOGIN_ROUTE } from "~/routes";
 import CreateAccount from "~/routes/signupPass/components";
 import { PASSWORD_FIELD } from "~/routes/signupPass/components/FormComponent";
+import { loginAccount } from "~/sequences/login";
 import { history } from "~/store";
 import actions, { HomeActions } from "./actions";
 import selectors, { SelectorProps } from "./selector";
@@ -34,19 +33,8 @@ class SignupPass extends React.Component<Props> {
   public readonly onCreateAccount = async (values: object) => {
     const { boot, drinkFaucet } = this.props;
     const password = (values as FormType)[PASSWORD_FIELD];
-    try {
-      const { accounts } = await boot(password, [config["chainSpec"]]);
-      const mainAccount = accounts[0];
-      const account = mainAccount ? mainAccount.account : undefined;
-      if (!account) {
-        await drinkFaucet(config["defaultFaucetUri"], config["faucetToken"]);
-      }
-    } catch (err) {
-      // TODO based on err show error
-      console.log(err);
-    }
 
-    history.push(SET_NAME_ROUTE);
+    await loginAccount(boot, drinkFaucet, password);
   };
 
   public render(): JSX.Element {
