35c5f056b26746de2022a4fa3c6501dd50fcfdde
Albert <albert.andrejev@gmail.com>
2018-12-17 21:25:41 +0200

    dropdown wrapper styling fix

--- src/components/subComponents/headers/dropdownWrapper.tsx
+++ src/components/subComponents/headers/dropdownWrapper.tsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+import { border, background, xs, md} from '~/theme/variables';
 
 export const HeaderDropdownWrapper = styled.div`
   width: 280px;
   box-shadow: 0 10px 7px 0 rgba(237, 239, 244, 0.14);
-  background-color: #ffffff;
-  border: 1px solid #f3f3f3;
-  border-radius: 0 0 4px 4px;
-  padding: 0px 15px;
+  background-color: ${background};
+  border: 1px solid ${border};
+  border-radius: 0 0 ${xs} ${xs};
+  padding: 0px ${md};
 `;
