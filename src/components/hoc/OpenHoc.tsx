e726ecba01bc823a2ef1f5e379cbf8ca58c72494
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-12-19 23:12:34 +0100

    Cleanup OpenHoc type definition

--- src/components/hoc/OpenHoc.tsx
+++ src/components/hoc/OpenHoc.tsx
@@ -1,10 +1,4 @@
-import {
-  InferableComponentEnhancerWithProps,
-  Omit,
-  StateHandler,
-  StateHandlerMap,
-  withStateHandlers,
-} from "recompose";
+import { Omit, StateHandler, StateHandlerMap, withStateHandlers } from "recompose";
 
 export interface OpenType {
   readonly open: boolean;
@@ -15,44 +9,16 @@ export interface OpenHandler extends StateHandlerMap<OpenType> {
   readonly clickAway: StateHandler<OpenType>;
 }
 
-/*
-export default withStateHandlers<OpenType, OpenHandler>(
-  { open: false },
-  {
-    toggle: (state: OpenType) => () => ({ open: !state.open }),
-    clickAway: (_: OpenType) => () => ({ open: false }),
-  },
-);
-*/
+type OpenHoc<P, T> = React.ComponentType<Omit<P, keyof P> & T>;
 
-/*
-export function openHoc<T extends {}>(): InferableComponentEnhancerWithProps<T & OpenType & OpenHandler, T> {
+export function openHoc<T>(
+  comp: React.ComponentType<T & OpenType & OpenHandler>,
+): OpenHoc<T & OpenType & OpenHandler, T> {
   return withStateHandlers<OpenType, OpenHandler, T>(
     { open: false },
     {
       toggle: (state: OpenType) => () => ({ open: !state.open }),
       clickAway: () => () => ({ open: false }),
     },
-  );
-}
-*/
-
-type OpenHoc<P, T> = React.ComponentType<Omit<P, keyof P> & T>;
-
-export function openHoc<T>(
-  comp: React.ComponentType<T & OpenType & OpenHandler>,
-): OpenHoc<T & OpenType & OpenHandler, T> {
-  const enhancer: InferableComponentEnhancerWithProps<T & OpenType & OpenHandler, T> = withStateHandlers<
-    OpenType,
-    OpenHandler,
-    T
-  >(
-    { open: false },
-    {
-      toggle: (state: OpenType) => () => ({ open: !state.open }),
-      clickAway: () => () => ({ open: false }),
-    },
-  );
-
-  return enhancer(comp);
+  )(comp);
 }
