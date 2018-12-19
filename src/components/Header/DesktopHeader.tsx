f6d2c85c66e7a10bf605141e9a4d626b8b84f241
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-12-19 14:44:49 +0100

    Added Icons with keyframe

--- src/components/Header/DesktopHeader.tsx
+++ src/components/Header/DesktopHeader.tsx
@@ -1,3 +1,52 @@
+import { createStyles, withStyles, WithStyles } from "@material-ui/core";
 import * as React from "react";
+import bell from "~/components/Header/assets/bell.svg";
+import loading from "~/components/Header/assets/loading.svg";
+import logoBlack from "~/components/Header/assets/logoBlack.svg";
+import Block from "~/components/layout/Block";
+import Img from "~/components/layout/Image";
 
-export default () => <React.Fragment />;
+const styles = createStyles({
+  root: {
+    display: 'flex',
+    flexWrap: 'nowrap',
+    height: '70px',
+    backgroundColor: 'white',
+  },
+  spin: {
+    animation: 'spinKeyframe 5s infinite linear',
+  },
+  '@keyframes spinKeyframe': {
+    '0%': {
+      transform: 'rotate(0deg)',
+    },
+    '50%': {
+      transform: 'rotate(180deg)',
+    },
+    '100%': {
+      transform: 'rotate(360deg)',
+    },
+  },
+  spacer: {
+    flexGrow: 1,
+  },
+  bell: {
+
+  },
+});
+
+interface Props extends WithStyles<typeof styles> { }
+
+const Header = ({ classes }: Props) => (
+  <React.Fragment>
+    <Block className={classes.root} padding="xxl">
+      <Img src={logoBlack} alt="Logo" />
+      <Block className={classes.spacer} />
+      {/* TODO refactor in #96 to include badge using IconGroup */}
+      <Img src={loading} className={classes.spin} alt="Loading Transactions" />
+      <Img src={bell} alt="Notifications" />
+    </Block>
+  </React.Fragment>
+);
+
+export default withStyles(styles)(Header);
