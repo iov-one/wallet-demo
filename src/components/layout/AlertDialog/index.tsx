17345192ac4da987da5f17758d3e47b4fcc02dca
Albert <albert.andrejev@gmail.com>
2018-12-20 11:21:10 +0200

    alert dialog refactoring

--- src/components/layout/AlertDialog/index.tsx
+++ src/components/layout/AlertDialog/index.tsx
@@ -4,58 +4,30 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import Typography from '@material-ui/core/Typography';
 
-import "./index.scss";
+import MainStyles from "./index.scss";
 
 
 const styles = (theme: Theme) => createStyles({
-  paper: {
-    overflow: "visible",
-  },
-  closeButton: {
-    position: 'absolute',
-    right: 0,
-    top: -40,
-  },
   closeIcon: {
     fill: theme.palette.primary.main,
   },
-  button: {
-    position: "relative",
-    top: 65,
-    width: "calc(100% - 60px)",
-    margin: "auto",
-  },
-  icon: {
-    margin: "auto",
-  },
-  iconParent: {
-    textAlign: "center",
-  },
-  message: {
-    fontSize: '1.2em',
-  }
 });
 
-interface AlertDialogProps extends WithStyles<typeof styles> {
+interface Props extends WithStyles<typeof styles> {
   readonly icon: string;
   readonly title: string;
+  readonly showDialog: boolean;
+  readonly onClose: () => any;
   readonly children: React.ReactNode;
 }
 
+interface State {
+  readonly open: boolean; 
+}
 
-class AlertDialog extends PureComponent<AlertDialogProps> {
+class AlertDialog extends PureComponent<Props, State> {
   public readonly state = {
     open: false,
-  };
-
-  public handleClickOpen = () => {
-    this.setState({
-      open: true,
-    });
-  };
-
-  public handleClose = () => {
-    this.setState({ open: false });
   };
 
   public render(): JSX.Element {
@@ -63,35 +35,35 @@ class AlertDialog extends PureComponent<AlertDialogProps> {
       classes,
       icon,
       title,
+      showDialog,
+      onClose,
       children,      
     } = this.props;
 
     return (
       <div>
-        <Button variant="outlined" color="secondary" onClick={this.handleClickOpen}>
-          Open dialog
-        </Button>
         <Dialog
-          onClose={this.handleClose}
-          classes={{ paper: classes.paper }}
+          onClose={onClose}
+          className={MainStyles["alert-dialog"]}
+          classes={{ paper: MainStyles.paper }}
           aria-labelledby="customized-dialog-title"
-          open={this.state.open}
+          open={showDialog}
         >
-          <svg className={classes.closeButton} xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="none" viewBox="0 0 26 26" onClick={this.handleClose}>
+          <svg className={MainStyles["close-button"]} xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="none" viewBox="0 0 26 26" onClick={onClose}>
             <path className={classes.closeIcon} fill="#D5D9DB" d="M1.543 0L0 1.543l.776.767L11.457 13 0 24.457 1.543 26 13 14.543l10.681 10.69.776.767L26 24.457l-.767-.776L14.543 13 26 1.543 24.457 0 13 11.457 2.31.776 1.543 0z" />
           </svg>
           <DialogContent>
-            <div className={classes.iconParent}>
+            <div className={MainStyles["icon-parent"]}>
               <img src={icon} />
             </div>
             <Typography gutterBottom variant="h4" align="center">
               {title}              
             </Typography>
-            <Typography gutterBottom align="center" className={classes.message}>
+            <Typography align="center" className={MainStyles.message}>
               {children}              
             </Typography>
           </DialogContent>
-          <Button onClick={this.handleClose} variant="contained" color="primary" className={classes.button}>
+          <Button onClick={onClose} variant="contained" color="primary" className={MainStyles.button}>
             Got it
           </Button>
         </Dialog>
