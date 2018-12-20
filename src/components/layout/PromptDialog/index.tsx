c23d83e5a1151747c2c418eb3477efeb7e90ded5
Albert <albert.andrejev@gmail.com>
2018-12-20 14:13:14 +0200

    prompt dialog fix

--- src/components/layout/PromptDialog/index.tsx
+++ src/components/layout/PromptDialog/index.tsx
@@ -1,6 +1,5 @@
 import React, { PureComponent } from "react";
 import Typography from '@material-ui/core/Typography';
-import { Form, Field } from 'react-final-form'
 
 import MainStyles from "./index.scss";
 import BaseDialog from "../BaseDialog";
@@ -21,10 +20,6 @@ class PromptDialog extends PureComponent<Props, State> {
   public readonly state = {
     open: false,
   };
-
-  public onSubmit = (values) => {
-    console.log(values);
-  }
   
 
   public render(): JSX.Element {
@@ -42,150 +37,7 @@ class PromptDialog extends PureComponent<Props, State> {
         onSubmit={onClose}
         submitButton="Continue"
       >
-        <h1>🏁 React Final Form - Simple Example</h1>
-        <a href="https://github.com/erikras/react-final-form#-react-final-form">
-          Read Docs
-    </a>
-        <Form
-          onSubmit={this.onSubmit}
-          initialValues={{ stooge: 'larry', employed: false }}
-          render={({ handleSubmit, form, submitting, pristine, values }) => (
-            <form onSubmit={handleSubmit}>
-              <div>
-                <label>First Name</label>
-                <Field
-                  name="firstName"
-                  component="input"
-                  type="text"
-                  placeholder="First Name"
-                />
-              </div>
-              <div>
-                <label>Last Name</label>
-                <Field
-                  name="lastName"
-                  component="input"
-                  type="text"
-                  placeholder="Last Name"
-                />
-              </div>
-              <div>
-                <label>Employed</label>
-                <Field name="employed" component="input" type="checkbox" />
-              </div>
-              <div>
-                <label>Favorite Color</label>
-                <Field name="favoriteColor" component="select">
-                  <option />
-                  <option value="#ff0000">❤️ Red</option>
-                  <option value="#00ff00">💚 Green</option>
-                  <option value="#0000ff">💙 Blue</option>
-                </Field>
-              </div>
-              <div>
-                <label>Toppings</label>
-                <Field name="toppings" component="select" multiple>
-                  <option value="chicken">🐓 Chicken</option>
-                  <option value="ham">🐷 Ham</option>
-                  <option value="mushrooms">🍄 Mushrooms</option>
-                  <option value="cheese">🧀 Cheese</option>
-                  <option value="tuna">🐟 Tuna</option>
-                  <option value="pineapple">🍍 Pineapple</option>
-                </Field>
-              </div>
-              <div>
-                <label>Sauces</label>
-                <div>
-                  <label>
-                    <Field
-                      name="sauces"
-                      component="input"
-                      type="checkbox"
-                      value="ketchup"
-                    />{' '}
-                    Ketchup
-              </label>
-                  <label>
-                    <Field
-                      name="sauces"
-                      component="input"
-                      type="checkbox"
-                      value="mustard"
-                    />{' '}
-                    Mustard
-              </label>
-                  <label>
-                    <Field
-                      name="sauces"
-                      component="input"
-                      type="checkbox"
-                      value="mayonnaise"
-                    />{' '}
-                    Mayonnaise
-              </label>
-                  <label>
-                    <Field
-                      name="sauces"
-                      component="input"
-                      type="checkbox"
-                      value="guacamole"
-                    />{' '}
-                    Guacamole 🥑
-              </label>
-                </div>
-              </div>
-              <div>
-                <label>Best Stooge</label>
-                <div>
-                  <label>
-                    <Field
-                      name="stooge"
-                      component="input"
-                      type="radio"
-                      value="larry"
-                    />{' '}
-                    Larry
-              </label>
-                  <label>
-                    <Field
-                      name="stooge"
-                      component="input"
-                      type="radio"
-                      value="moe"
-                    />{' '}
-                    Moe
-              </label>
-                  <label>
-                    <Field
-                      name="stooge"
-                      component="input"
-                      type="radio"
-                      value="curly"
-                    />{' '}
-                    Curly
-              </label>
-                </div>
-              </div>
-              <div>
-                <label>Notes</label>
-                <Field name="notes" component="textarea" placeholder="Notes" />
-              </div>
-              <div className="buttons">
-                <button type="submit" disabled={submitting || pristine}>
-                  Submit
-            </button>
-                <button
-                  type="button"
-                  onClick={form.reset}
-                  disabled={submitting || pristine}
-                >
-                  Reset
-            </button>
-              </div>
-              <pre>{JSON.stringify(values, 0, 2)}</pre>
-            </form>
-          )}
-        />
+        <h1>Prompt dialog content should be here</h1>        
       </BaseDialog >
     );
   }
