f062117e64a90f115a232c8c31826edeff6f0fd6
Albert <albert.andrejev@gmail.com>
2018-12-24 21:48:17 +0200

    set password dialog implementation

--- src/routes/securityCenter/components/SetPassword.tsx
+++ src/routes/securityCenter/components/SetPassword.tsx
@@ -1,42 +1,59 @@
+import { FormState, FormSubscription } from "final-form";
 import React from "react";
 import Field from "~/components/forms/Field";
+import Form from "~/components/forms/Form";
 import TextField from "~/components/forms/TextField";
+import { required } from "~/components/forms/validator";
 import Block from "~/components/layout/Block";
+import Button from "~/components/layout/Button";
 import { Prompt } from "~/components/layout/dialogs";
 import Typography from "~/components/layout/Typography";
 
 interface Props {
   readonly showSetPassword: boolean;
   readonly closeSetPassword: () => void;
+  readonly onSubmit: (values: object) => void;
+  readonly validation?: (values: object) => object | Promise<object>;
 }
 
-export default ({ showSetPassword, closeSetPassword }: Props): JSX.Element => (
-  <Prompt
-    showDialog={showSetPassword}
-    onClose={closeSetPassword}
-  >
-    <React.Fragment>
-    <Block padding="xxl" maxWidth={450} margin="xxl">
-      <Block margin="sm">
-        <Typography variant="subtitle2" color="textPrimary">
-          Password
-        </Typography>
-      </Block>
-      <Field
-        variant="outlined"
-        name={LOGIN_PASS_FIELD}
-        type="password"
-        fullWidth
-        component={TextField}
-        validate={required}
-        placeholder="Your password"
-      />
-    </Block>
-    <Block padding="xxl" maxWidth={450} margin="xl">
-      <Typography variant="subtitle1" color="primary" underlined>
-        Forgot your password?
-      </Typography>
-    </Block>
-  </React.Fragment>
+const subscription: FormSubscription = {
+  valid: true,
+  submitting: true,
+  validating: true,
+};
+
+export default ({ showSetPassword, closeSetPassword, onSubmit, validation }: Props): JSX.Element => (
+  <Prompt showDialog={showSetPassword} onClose={closeSetPassword}>
+    <Form onSubmit={onSubmit} subscription={subscription} validation={validation} grow>
+      {({ valid, submitting, validating }: FormState) => (
+        <React.Fragment>
+          <Block padding="xxl" maxWidth={450} margin="xxl">
+            <Block margin="sm">
+              <Typography variant="subtitle2" color="textPrimary">
+                Password
+              </Typography>
+            </Block>
+            <Field
+              variant="outlined"
+              name="field_1"
+              type="password"
+              fullWidth
+              component={TextField}
+              validate={required}
+              placeholder="Your password"
+            />
+          </Block>
+          <Button
+            variant="continue"
+            color="primary"
+            type="submit"
+            disabled={!valid || submitting || validating}
+            size="large"
+          >
+            Got it
+          </Button>
+        </React.Fragment>
+      )}
+    </Form>
   </Prompt>
 );
