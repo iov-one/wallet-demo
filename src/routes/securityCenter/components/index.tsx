8d10c4372e88afed4f146e23b59adc60b5ca2ea4
Albert <albert.andrejev@gmail.com>
2018-12-24 16:16:23 +0200

    extra security component

--- src/routes/securityCenter/components/index.tsx
+++ src/routes/securityCenter/components/index.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { SuggestionButton } from "~/components/subComponents/buttons";
 import BPIcon from "../assets/backupPhrase.svg";
 import PswIcon from "../assets/password.svg";
 import AdvancedSecurity from "./AdvancedSecurity";
+import { ExtraSecurity } from "./ExtraSecurity";
 import SecurityCard from "./ItemCard";
 import PageTitle from "./PageTitle";
-import Block from "~/components/layout/Block";
-import Typography from "~/components/layout/Typography";
 
 interface Props {
   readonly showAdvancedSecurity: boolean;
@@ -26,11 +24,6 @@ export default ({ onAdvancedSecurity, showAdvancedSecurity, closeAdvancedSecurit
       showAdvancedSecurity={showAdvancedSecurity}
       closeAdvancedSecurity={closeAdvancedSecurity}
     />
-    <Block margin="xs">
-      <Typography variant="subtitle1" color="textPrimary">Extra security?</Typography>
-    </Block>
-    <Block margin="lg">
-      <Typography variant="subtitle1" color="primary" underlined onClick={onAdvancedSecurity}>See advanced security</Typography>
-    </Block>
+    <ExtraSecurity onClick={onAdvancedSecurity} />
   </React.Fragment>
 );
