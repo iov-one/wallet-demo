acfc19f76f903cda14135ad88f05e3c2cbba4a35
Adolfo Panizo <adolfo.panizo@gmail.com>
2018-12-26 09:58:00 +0100

    Create PageMenu layout component

---
+++ src/components/pages/PageMenu/index.tsx
@@ -0,0 +1,27 @@
+import { createStyles, withStyles, WithStyles } from "@material-ui/core";
+import * as React from "react";
+import Header from "~/components/Header";
+import Grid from "~/components/layout/Grid";
+import GridItem from "~/components/layout/GridItem";
+import { backgroundPrimary } from "~/theme/variables";
+
+interface Props extends WithStyles<typeof styles> {
+  readonly children: React.ReactNode;
+}
+
+const styles = createStyles({
+  root: {
+    backgroundColor: backgroundPrimary
+  }
+}) 
+
+const PageMenu = ({ children, classes }: Props) => (
+  <Grid root className={classes.root}>
+    <Header />
+    <GridItem xs={12} variant="column" grow middle="xs">
+      {children}
+    </GridItem>
+  </Grid>
+);
+
+export default withStyles(styles)(PageMenu);
