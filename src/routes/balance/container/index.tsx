b2176257f8e2ec7db511417b3d7a1359b17ce54e
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-01-02 10:53:17 +0100

    Adding balance container

---
+++ src/routes/balance/container/index.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import PageMenu from "~/components/pages/PageMenu";
+// import Layout from "~/routes/balance/components";
+
+class Balance extends React.Component {
+  public render(): JSX.Element {
+    return (
+      <PageMenu>
+        {/* <Layout /> */}
+      </PageMenu>
+    );
+  }
+}
+
+export default Balance;
