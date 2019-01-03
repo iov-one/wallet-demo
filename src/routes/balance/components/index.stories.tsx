f90d01fca9f5472d2b8e0a1b5873a9f7425fbbcf
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-01-03 14:12:49 +0100

    Fix storybook stories for balance view

--- src/routes/balance/components/index.stories.tsx
+++ src/routes/balance/components/index.stories.tsx
@@ -1,6 +1,7 @@
 import { BcpCoin, TokenTicker } from "@iov/bcp-types";
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
+import PageMenu from "~/components/pages/PageMenu";
 import { RootMatchMedia } from "~/utils/storybook";
 import Layout from "./index";
 
@@ -23,8 +24,20 @@ const TOKENS: ReadonlyArray<BcpCoin> = [
 
 const ACCOUNT_NAME = "adolfo*iov";
 
-storiesOf("Routes /balance", module).add("Balance view", () => (
-  <RootMatchMedia matchMedia={false}>
-    <Layout name={ACCOUNT_NAME} tokens={TOKENS} />
-  </RootMatchMedia>
-));
+storiesOf("Routes /balance", module)
+  .add("Balance view for desktop", () => (
+    <RootMatchMedia matchMedia={false}>
+      <PageMenu phoneFullWidth>
+        {(phone: boolean) => <Layout name={ACCOUNT_NAME} tokens={TOKENS} phone={phone} />}
+      </PageMenu>
+    </RootMatchMedia>
+  ))
+  .add("Balance view for phones", () => (
+    <RootMatchMedia matchMedia={true}>
+      <div style={{ maxWidth: "360px" }}>
+        <PageMenu phoneFullWidth>
+          {(phone: boolean) => <Layout name={ACCOUNT_NAME} tokens={TOKENS} phone={phone} />}
+        </PageMenu>
+      </div>
+    </RootMatchMedia>
+  ));
