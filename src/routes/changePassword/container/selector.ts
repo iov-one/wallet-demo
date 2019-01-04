126cd6727f8c59b9f28099215b79ccf875940fa3
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-01-04 12:33:31 +0100

    Adding profile selector

--- src/routes/changePassword/container/selector.ts
+++ src/routes/changePassword/container/selector.ts
@@ -1,14 +1,18 @@
+import { UserProfile } from "@iov/core";
 import { createStructuredSelector, Selector } from "reselect";
 import { StringDB } from "~/logic";
 import { RootState } from "~/reducers";
 import { dbSelector } from "~/routes/home/container/selector";
+import { getProfile } from "~/selectors";
 
 export interface SelectorProps {
   readonly db: StringDB;
+  readonly profile: UserProfile | undefined;
 }
 
 const structuredSelector: Selector<RootState, SelectorProps> = createStructuredSelector({
   db: dbSelector,
+  profile: getProfile,
 });
 
 export default structuredSelector;
