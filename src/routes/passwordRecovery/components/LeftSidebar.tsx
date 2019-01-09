c5297c7c02c059f697445bb003890b8a0f2a20f1
Albert <albert.andrejev@gmail.com>
2019-01-09 16:12:57 +0200

    styles fix

--- src/routes/passwordRecovery/components/LeftSidebar.tsx
+++ src/routes/passwordRecovery/components/LeftSidebar.tsx
@@ -2,32 +2,13 @@ import { createStyles, WithStyles, withStyles } from "@material-ui/core";
 import * as React from "react";
 import Block from "~/components/layout/Block";
 import Spacer from "~/components/layout/Spacer";
-import { background, md, xl, xxl } from "~/theme/variables";
 
 const styles = createStyles({
-  noticeBox: {
-    display: "flex",
-    flexDirection: "column",
-    opacity: 0.7,
-    borderRadius: 5,
-    padding: xl,
-    margin: xxl,
-    boxShadow: "0 0 20px 0 rgba(237, 237, 237, 0.44)",
-    backgroundImage: `linear-gradient(to top, ${background}, rgba(255, 255, 255, 0.71))`,
-  },
   leftSide: {
     display: "flex",
     flexDirection: "column",
-    width: "100%",
     height: "100vh",
     backgroundImage: "linear-gradient(to top, #ecf4f3, #cdeae7)",
-  },
-  header: {
-    display: "flex",
-    alignItems: "center",
-  },
-  bulbIcon: {
-    marginRight: md,
   },
 });
 
