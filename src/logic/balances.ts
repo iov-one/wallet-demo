140d2ea52cca2610b73f84f982b910a0413b63fe
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-01-09 13:12:06 +0100

    Re-wrote balance parsing for new amount

--- src/logic/balances.ts
+++ src/logic/balances.ts
@@ -1,28 +1,7 @@
-export function stringToFractional(fractionString: string, sigFigs: number): number {
-  // we ensure no more than sigFigs are parsed
-  const cleanFrac = fractionString.slice(0, sigFigs);
-  return parseInt(cleanFrac.padEnd(sigFigs, "0"), 10);
-}
+import { Amount, TokenTicker } from "@iov/bcp-types";
 
-export function fractionalToString(fractional: number, sigFigs: number): string {
-  const fraction = fractional.toString().padStart(sigFigs, "0");
-  if (fraction.length > sigFigs) {
-    throw new Error("Number too large for the given sigFigs");
-  }
-  const [, trailingZeros] = fraction.match(/(0*)$/)!;
-  if (trailingZeros !== "") {
-    return fraction.slice(0, -trailingZeros.length);
-  }
-  return fraction;
-}
-
-export interface CoinInfo {
-  readonly whole: number;
-  readonly fractional: number;
-  readonly sigFigs: number;
-}
-
-export function stringToCoin(amount: string, sigFigs: number): CoinInfo {
+// This parses a decimal as string into the Amount format
+export function stringToAmount(amount: string, tokenTicker: TokenTicker): Amount {
   const matched = amount.match(/^([0-9]+)?([\.\,]([0-9]+))?$/);
   if (!matched) {
     throw new Error(`Not a valid number: ${amount}`);
@@ -30,18 +9,60 @@ export function stringToCoin(amount: string, sigFigs: number): CoinInfo {
   // elements 1 and 3...
   const wholeString = matched[1];
   const fractionString = matched[3];
-  const whole = wholeString ? parseInt(wholeString, 10) : 0;
-  const fractional = fractionString ? stringToFractional(fractionString, sigFigs) : 0;
-  return { whole, fractional, sigFigs };
+  const quantity = `${wholeString}${fractionString}`;
+  const fractionalDigits = fractionString.length;
+  return { quantity, fractionalDigits, tokenTicker };
 }
 
-export function coinToString(coin: CoinInfo): string {
-  if (coin.fractional < 0 || coin.whole < 0) {
-    throw new Error("Coin value must be non-negative");
+// This produces a human readable format of the amount, value and token ticker
+export function amountToString(amount: Amount): string {
+  const { quantity, fractionalDigits, tokenTicker } = amount;
+  if (!quantity.match(/^[0-9]+$/)) {
+    throw new Error(`quantity must be a number, got ${quantity}`);
   }
-  if (coin.fractional === 0) {
-    return `${coin.whole}`;
+  if (fractionalDigits < 0) {
+    throw new Error(`invalid fractional digits: ${fractionalDigits}`);
   }
-  const fractionString = fractionalToString(coin.fractional, coin.sigFigs);
-  return `${coin.whole}.${fractionString}`;
+
+  if (fractionalDigits > quantity.length) {
+    // TODO: left pad if length is shorter
+    throw new Error("TODO");
+  }
+  const wholeDigits = quantity.length - fractionalDigits;
+  const value = `${quantity.slice(0, wholeDigits)}.${quantity.slice(wholeDigits)} ${tokenTicker}`;
+  return value;
+}
+
+// this takes an amount and trims off all trailing 0s
+export function trimAmount(amount: Amount): Amount {
+  const { quantity, fractionalDigits, tokenTicker } = amount;
+  const zeros = quantity.match(/0*$/)![0].length;
+  if (zeros === 0) {
+    return amount;
+  }
+  const trimmedQuantity = quantity.slice(0, -zeros);
+  const trimmedDigits = fractionalDigits - zeros;
+  return {
+    quantity: trimmedQuantity,
+    fractionalDigits: trimmedDigits,
+    tokenTicker,
+  };
+}
+
+// this takes an amount and pad 0s to the desired fractionalDigits, or throws error if fractionalDigits is already larger
+export function padAmount(amount: Amount, desiredDigits: number): Amount {
+  const { quantity, fractionalDigits, tokenTicker } = amount;
+  const diff = desiredDigits - fractionalDigits;
+  if (diff < 0) {
+    throw new Error(`Want to pad to ${desiredDigits}, but already has ${fractionalDigits}`);
+  } else if (diff === 0) {
+    return amount;
+  } else {
+    const newQuantity = quantity + "0".repeat(diff);
+    return {
+      quantity: newQuantity,
+      fractionalDigits: desiredDigits,
+      tokenTicker,
+    };
+  }
 }
