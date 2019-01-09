da6d7c9905f5edeefe847c9b0540ca38976010e6
Albert <albert.andrejev@gmail.com>
2019-01-09 18:33:57 +0200

    storybook fix (again)

--- src/routes/passwordRecovery/container/index.stories.js
+++ src/routes/passwordRecovery/container/index.stories.js
@@ -3,13 +3,13 @@ import * as React from "react";
 import { RootMatchMedia } from "~/utils/storybook";
 import PasswordRecovery from "./index";
 
-storiesOf("Routes /security-center", module)
-  .add("Security center for desktop", () => (
+storiesOf("Routes /password-recovery", module)
+  .add("Password Recovery for desktop", () => (
     <RootMatchMedia matchMedia={false}>
       <PasswordRecovery />
     </RootMatchMedia>
   ))
-  .add("Security center for phones", () => (
+  .add("Password Recovery for phones", () => (
     <RootMatchMedia matchMedia={true}>
       <PasswordRecovery />
     </RootMatchMedia>
