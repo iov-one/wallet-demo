c0e2d71e123259b2765a87f67e00509fd6bbdd85
Albert <albert.andrejev@gmail.com>
2019-01-09 18:29:15 +0200

    Password recovery story fix

--- src/routes/passwordRecovery/container/index.stories.js
+++ src/routes/passwordRecovery/container/index.stories.js
@@ -1,16 +1,16 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 import { RootMatchMedia } from "~/utils/storybook";
-import SecurityCenter from "./index";
+import PasswordRecovery from "./index";
 
 storiesOf("Routes /security-center", module)
   .add("Security center for desktop", () => (
     <RootMatchMedia matchMedia={false}>
-      <SecurityCenter />
+      <PasswordRecovery />
     </RootMatchMedia>
   ))
   .add("Security center for phones", () => (
     <RootMatchMedia matchMedia={true}>
-      <SecurityCenter />
+      <PasswordRecovery />
     </RootMatchMedia>
   ));
