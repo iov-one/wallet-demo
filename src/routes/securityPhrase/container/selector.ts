e63c3f997f97d3fc7c42a1fd0f6a1cdd3bcd944e
Albert Andrejev <albert.andrejev@gmail.com>
2019-01-11 18:38:44 +0200

    remove unused selectors and use getActiveWallet selector

--- src/routes/securityPhrase/container/selector.ts
+++ src/routes/securityPhrase/container/selector.ts
@@ -1,30 +1,21 @@
 import { UserProfile } from "@iov/core";
-import { WalletInfo } from "@iov/keycontrol";
 import { createSelector, createStructuredSelector, Selector } from "reselect";
 import { RootState } from "~/reducers";
-import { getProfile } from "~/selectors";
+import { ActiveIdentity } from "~/reducers/profile";
+import { getActiveWallet, getProfile } from "~/selectors";
 
 export interface SelectorProps {
-  readonly profile: UserProfile | undefined;
-  readonly wallet: WalletInfo | undefined;
   readonly mnemonic: string | undefined;
 }
 
-const getProfileWallet: (state: RootState) => WalletInfo | undefined = createSelector(
-  getProfile,
-  (profile: UserProfile | undefined) => (profile === undefined ? undefined : profile.wallets.value[0]),
-);
-
 const getWalletMnemonic: (state: RootState) => string | undefined = createSelector(
   getProfile,
-  getProfileWallet,
-  (profile: UserProfile | undefined, wallet: WalletInfo | undefined) =>
-    profile === undefined || wallet === undefined ? undefined : profile.printableSecret(wallet.id),
+  getActiveWallet,
+  (profile: UserProfile | undefined, wallet: ActiveIdentity | undefined) =>
+    profile === undefined || wallet === undefined ? undefined : profile.printableSecret(wallet.walletId),
 );
 
 const structuredSelector: Selector<RootState, SelectorProps> = createStructuredSelector({
-  profile: getProfile,
-  wallet: getProfileWallet,
   mnemonic: getWalletMnemonic,
 });
 
