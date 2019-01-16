03d8456684b70c2572b65a2056b2132c59597002
Albert Andrejev <albert.andrejev@gmail.com>
2019-01-16 11:17:09 +0200

    Review fixes

--- stories/form.stories.js
+++ stories/form.stories.js
@@ -1,18 +1,10 @@
 import React from "react";
 
-import WebFont from "webfontloader";
-
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import "@storybook/addon-actions/register";
 
 import { AddressInputForm, BalanceForm } from "../src/components/templates/forms";
-
-WebFont.load({
-  google: {
-    families: ["Muli:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"],
-  },
-});
 
 storiesOf("Forms", module)
   .add("Balance Form", () => {
