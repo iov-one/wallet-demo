a032757dc09e641df990a01705e1dc880989dc07
Albert Andrejev <albert.andrejev@gmail.com>
2019-01-17 11:09:01 +0200

    Process login without IOV username

---
+++ src/routes/login/container/login.dom.spec.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import TestUtils from "react-dom/test-utils";
+import { Store } from "redux";
+import { mayTest, randomString } from "~/logic/testhelpers";
+import { RootState } from "~/reducers";
+import { LOGIN_ROUTE, SET_NAME_ROUTE } from "~/routes";
+import { signUp } from "~/routes/signupPass/container/signup.dom.spec";
+import { shutdownSequence } from "~/sequences";
+import { aNewStore, history } from "~/store";
+import { sleep } from "~/utils/timer";
+
+export const travelToLogin = async (store: Store, password: string): Promise<React.Component> => {
+  const signUpDom = await signUp(store, password);
+  history.push(LOGIN_ROUTE);
+
+  return signUpDom;
+};
+
+describe("DOM > Feature > Login", () => {
+  let store: Store<RootState>;
+  let walletDom: React.Component;
+  let profilePass: string;
+
+  beforeEach(async (done) => {
+    profilePass = randomString(16);
+    store = aNewStore();
+    walletDom = await travelToLogin(store, profilePass);
+    done();
+  });
+
+  afterEach(() => {
+    shutdownSequence(null, store.getState);
+  });
+
+  mayTest("should contain only one field for password", async () => {
+    const inputs = TestUtils.scryRenderedDOMComponentsWithTag(walletDom, "input");
+    expect(inputs.length).toBe(1);
+  });
+
+  mayTest(`should redirect to ${SET_NAME_ROUTE} route after success login`, async (done) => {
+    const inputs = TestUtils.scryRenderedDOMComponentsWithTag(walletDom, "input");
+
+    const password = inputs[0];
+    TestUtils.Simulate.change(password, { target: { value: profilePass } } as any);
+    
+    const form = TestUtils.findRenderedDOMComponentWithTag(walletDom, "form");
+    TestUtils.Simulate.submit(form);
+
+    await sleep(3000);
+    expect(store.getState().router.location.pathname).toBe(SET_NAME_ROUTE);
+    done();
+  });
+});
