00d47dc58396e430570ff1355cd4ba306ce75159
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-01-17 18:42:14 +0100

    Login test just creates profile and then validates it goes to login

--- src/routes/login/container/login.dom.spec.tsx
+++ src/routes/login/container/login.dom.spec.tsx
@@ -1,21 +1,42 @@
+import { ConnectedRouter } from "connected-react-router";
 import * as React from "react";
 import TestUtils from "react-dom/test-utils";
+import { Provider } from "react-redux";
 import { Store } from "redux";
+
 import { mayTestBns, randomString } from "~/logic/testhelpers";
 import { RootState } from "~/reducers";
-import { LOGIN_ROUTE, SET_NAME_ROUTE } from "~/routes";
-import { signUp } from "~/routes/signupPass/container/signup.dom.spec";
+import { fixTypes } from "~/reducers/helpers";
+import { createProfileAsyncAction } from "~/reducers/profile";
+import Route, { LOGIN_ROUTE, SET_NAME_ROUTE } from "~/routes";
+import { getProfileDB } from "~/selectors";
 import { shutdownSequence } from "~/sequences";
 import { aNewStore, history } from "~/store";
 import { sleep } from "~/utils/timer";
 
-jest.setTimeout(100000);
+// TODO: this is copied from signup.dom.spec.tsx 
+// this function should probably be pulled into a helper file somewhere, but I don't know proper location
+const createDom = (store: Store): React.Component<any, any, any> =>
+  TestUtils.renderIntoDocument(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Route />
+      </ConnectedRouter>
+    </Provider>,
+  ) as React.Component;
 
-export const travelToLogin = async (store: Store, password: string): Promise<React.Component> => {
-  const signUpDom = await signUp(store, password);
+export const travelToLogin = async (store: Store, profilePass: string): Promise<React.Component<{}>> => {
+  // --- initialize the profile
+  const db = getProfileDB(store.getState());
+  const { value: profile } = await fixTypes(
+    store.dispatch(createProfileAsyncAction.start(db, profilePass, undefined)),
+  );
+  expect(profile).toBeDefined();
+  expect(profile.wallets.value.length).toBe(1);
+
+  // now, go to login with existing profile
   history.push(LOGIN_ROUTE);
-
-  return signUpDom;
+  return createDom(store);
 };
 
 describe("DOM > Feature > Login", () => {
@@ -25,7 +46,7 @@ describe("DOM > Feature > Login", () => {
 
   beforeEach(async () => {
     profilePass = randomString(16);
-    store = aNewStore();    
+    store = aNewStore();
 
     walletDom = await travelToLogin(store, profilePass);
   });
@@ -34,20 +55,23 @@ describe("DOM > Feature > Login", () => {
     shutdownSequence(null, store.getState);
   });
 
-  mayTestBns(`should redirect to ${SET_NAME_ROUTE} route after success login`, async () => {
-    const inputs = TestUtils.scryRenderedDOMComponentsWithTag(walletDom, "input");
+  mayTestBns(
+    `should redirect to ${SET_NAME_ROUTE} route after success login`,
+    async () => {
+      const inputs = TestUtils.scryRenderedDOMComponentsWithTag(walletDom, "input");
 
-    const password = inputs[0];
-    TestUtils.Simulate.change(password, { target: { value: profilePass } } as any);
+      const password = inputs[0];
+      TestUtils.Simulate.change(password, { target: { value: profilePass } } as any);
 
-    const form = TestUtils.findRenderedDOMComponentWithTag(walletDom, "form");
-    if (!form) {
-      throw new Error();
-    }
-    TestUtils.Simulate.submit(form);
-    
-    await sleep(3000);
-    //expect(store.getState().router.location.pathname).toBe(SET_NAME_ROUTE);
-    expect(true).toBe(true);
-  });
+      const form = TestUtils.findRenderedDOMComponentWithTag(walletDom, "form");
+      if (!form) {
+        throw new Error();
+      }
+      TestUtils.Simulate.submit(form);
+
+      await sleep(3000);
+      expect(store.getState().router.location.pathname).toBe(SET_NAME_ROUTE);
+    },
+    10000,
+  );
 });
--- src/routes/signupPass/container/signup.dom.spec.tsx
+++ src/routes/signupPass/container/signup.dom.spec.tsx
@@ -25,25 +25,6 @@ export const travelToSignup = (store: Store): React.Component<{}> => {
   return createDom(store);
 };
 
-export const signUp = async (store: Store, userPassword: string): Promise<React.Component> => {
-  const signUpDom = await travelToSignup(store);
-  const inputs = TestUtils.scryRenderedDOMComponentsWithTag(signUpDom, "input");
-  const email = inputs[0];
-  TestUtils.Simulate.change(email, { target: { value: "foo@bar.com" } } as any);
-  const password = inputs[1];
-  TestUtils.Simulate.change(password, { target: { value: userPassword } } as any);
-  const repeatPassword = inputs[2];
-  TestUtils.Simulate.change(repeatPassword, { target: { value: userPassword } } as any);
-  const accept = inputs[3];
-  TestUtils.Simulate.change(accept, { target: { value: "true" } } as any);
-  const form = TestUtils.findRenderedDOMComponentWithTag(signUpDom, "form");
-  TestUtils.Simulate.submit(form);
-
-  await sleep(3000);  
-  return signUpDom;
-}
-
-
 describe("DOM > Feature > Signup", () => {
   let store: Store<RootState>;
   beforeEach(() => {
@@ -52,33 +33,37 @@ describe("DOM > Feature > Signup", () => {
   afterEach(() => {
     shutdownSequence(null, store.getState);
   });
-  mayTestBns("creates account after filling form", async () => {
-    const SignUpDom = await travelToSignup(store);
+  mayTestBns(
+    "creates account after filling form",
+    async () => {
+      const SignUpDom = await travelToSignup(store);
 
-    // Let's fill the form
-    const inputs = TestUtils.scryRenderedDOMComponentsWithTag(SignUpDom, "input");
-    expect(inputs.length).toBe(4);
+      // Let's fill the form
+      const inputs = TestUtils.scryRenderedDOMComponentsWithTag(SignUpDom, "input");
+      expect(inputs.length).toBe(4);
 
-    const email = inputs[0];
-    TestUtils.Simulate.change(email, { target: { value: "foo@bar.com" } } as any);
+      const email = inputs[0];
+      TestUtils.Simulate.change(email, { target: { value: "foo@bar.com" } } as any);
 
-    const password = inputs[1];
-    TestUtils.Simulate.change(password, { target: { value: "pass phrase here" } } as any);
+      const password = inputs[1];
+      TestUtils.Simulate.change(password, { target: { value: "pass phrase here" } } as any);
 
-    const repeatPassword = inputs[2];
-    TestUtils.Simulate.change(repeatPassword, { target: { value: "pass phrase here" } } as any);
+      const repeatPassword = inputs[2];
+      TestUtils.Simulate.change(repeatPassword, { target: { value: "pass phrase here" } } as any);
 
-    const accept = inputs[3];
-    TestUtils.Simulate.change(accept, { target: { value: "true" } } as any);
+      const accept = inputs[3];
+      TestUtils.Simulate.change(accept, { target: { value: "true" } } as any);
 
-    // Let's submit the form
-    const form = TestUtils.findRenderedDOMComponentWithTag(SignUpDom, "form");
-    if (!form) {
-      throw new Error();
-    }
-    TestUtils.Simulate.submit(form);
+      // Let's submit the form
+      const form = TestUtils.findRenderedDOMComponentWithTag(SignUpDom, "form");
+      if (!form) {
+        throw new Error();
+      }
+      TestUtils.Simulate.submit(form);
 
-    await sleep(3000);
-    expect(store.getState().router.location.pathname).toBe(SET_NAME_ROUTE);
-  });
+      await sleep(3000);
+      expect(store.getState().router.location.pathname).toBe(SET_NAME_ROUTE);
+    },
+    10000,
+  );
 });
