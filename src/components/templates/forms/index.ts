fc694575488a0c220194ec26f54a9ae2a473cabf
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-01-21 16:09:50 +0100

    Remove unused forms in old containers

--- src/components/templates/forms/index.ts
+++ src/components/templates/forms/index.ts
@@ -1,11 +1,2 @@
-export * from "./balanceForm";
-export * from "./addressInputForm";
 export * from "./receiveIOVForm";
 export * from "./receiveNonIOVForm";
-
-/* below will be replaced */
-export * from "./createWalletForm";
-export * from "./setPasswordForm";
-export * from "./loginForm";
-export * from "./importAccountForm";
-export * from "./backupAccountForm";
