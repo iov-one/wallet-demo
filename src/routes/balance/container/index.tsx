f7d94769dfbfb5bc44d368c41f584e2705ff5278
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-01-21 08:26:55 +0100

    Adding IOV namespace to balance route

--- src/routes/balance/container/index.tsx
+++ src/routes/balance/container/index.tsx
@@ -6,23 +6,26 @@ import Layout from "~/routes/balance/components";
 import { history } from "~/store";
 import selector, { SelectorProps } from "./selector";
 
+export const IOV_NAMESPACE = "*iov";
+
 class Balance extends React.Component<SelectorProps> {
+  public readonly onSendPayment = () => {
+    history.push(PAYMENT_ROUTE);
+  };
+
+  public readonly onReceivePayment = () => {
+    history.push(RECEIVE_FROM_IOV_USER);
+  };
+
   public render(): JSX.Element {
     const { name, tokens } = this.props;
-
-    const onSendPayment = () => {
-      history.push(PAYMENT_ROUTE);
-    };
-
-    const onReceivePayment = () => {
-      history.push(RECEIVE_FROM_IOV_USER);
-    };
+    const iovAddress = `${name}${IOV_NAMESPACE}`;
 
     const renderProps = (phone: boolean) => (
       <Layout
-        onSendPayment={onSendPayment}
-        onReceivePayment={onReceivePayment}
-        name={name}
+        onSendPayment={this.onSendPayment}
+        onReceivePayment={this.onReceivePayment}
+        name={iovAddress}
         tokens={tokens}
         phone={phone}
       />
--- src/routes/receiveIov/container/index.tsx
+++ src/routes/receiveIov/container/index.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import PageMenu from "~/components/pages/PageMenu";
 import { SuggestionButton } from "~/components/subComponents/buttons";
 import { RECEIVE_FROM_NON_IOV_USER } from "~/routes";
+import { IOV_NAMESPACE } from "~/routes/balance/container";
 import ReceiveIovLayout from "~/routes/receiveIov/components";
 import { history } from "~/store";
 import selector, { SelectorProps } from "./selector";
@@ -13,8 +14,6 @@ const Layout = styled.div`
   justify-content: center;
 `;
 
-const IOV_NAMESPACE = "*iov"
-
 class RecieveIov extends React.Component<SelectorProps> {
   public readonly onReceiveExpernal = () => {
     history.push(RECEIVE_FROM_NON_IOV_USER);
@@ -22,7 +21,7 @@ class RecieveIov extends React.Component<SelectorProps> {
 
   public render(): JSX.Element {
     const { accountName } = this.props;
-    const iovAddress = accountName ? `${accountName}${IOV_NAMESPACE}` : "--"
+    const iovAddress = accountName ? `${accountName}${IOV_NAMESPACE}` : "--";
 
     return (
       <PageMenu phoneFullWidth>
