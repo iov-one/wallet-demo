d07c0effa629553429efa5d31c5f890dd385a989
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-01-21 16:39:18 +0100

    Removing unused storybook stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,12 +4,11 @@ import { configure } from '@storybook/react';
 const reqRoutes = require.context('../src/routes', true, /\.stories\.((js|ts)x?)$/);
 const reqComponents = require.context('../src/components', true, /\.stories\.((js|ts)x?)$/);
 const ctxComponents = require.context('../src/context', true, /\.stories\.((js|ts)x?)$/);
-const req = require.context('../stories', true, /.stories.js$/);
 
 function loadStories() {
   reqRoutes.keys().forEach((filename) => reqRoutes(filename));
   reqComponents.keys().forEach((filename) => reqComponents(filename));
-  req.keys().forEach(filename => req(filename));
+  ctxComponents.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
