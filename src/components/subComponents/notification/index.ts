f42e341d15a8796d492bc36282af897a2127bb23
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-01-21 16:15:34 +0100

    Removing notification and typography components

--- src/components/subComponents/notification/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./notificationWrapper";
-export * from "./pendingTransactionNotification";
-export * from "./notificationTitle";
-export * from "./transaction";
-export * from "./pendingTransaction";
-export * from "./notificationEmptyState";
