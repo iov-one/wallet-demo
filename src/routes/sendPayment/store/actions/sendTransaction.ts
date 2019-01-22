9bf891ded09091322ddc11b1fff724f91de669df
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-01-22 12:00:47 +0100

    Adding signer address to sendTransaction action

--- src/routes/sendPayment/store/actions/sendTransaction.ts
+++ src/routes/sendPayment/store/actions/sendTransaction.ts
@@ -8,8 +8,15 @@ export type SendTxType = (
   amount: Amount,
   note: string,
   id: string,
+  accountName: string,
 ) => Promise<void>;
 
-export default (chainId: ChainId, address: string, amount: Amount, note: string, id: string) => async (
-  dispatch: any,
-): Promise<void> => dispatch(sendTransactionSequence(chainId, address, amount, note, id));
+export default (
+  chainId: ChainId,
+  address: string,
+  amount: Amount,
+  note: string,
+  id: string,
+  accountName: string,
+) => async (dispatch: any): Promise<void> =>
+  dispatch(sendTransactionSequence(chainId, address, amount, note, id, accountName));
