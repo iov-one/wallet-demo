76b431690bc7c948a2060fa3632ea09e5d6ddf73
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-01-23 12:48:57 +0100

    Giving more time to validate name

--- src/routes/signupName/test/utils/travelSetName.ts
+++ src/routes/signupName/test/utils/travelSetName.ts
@@ -4,12 +4,14 @@ import { sleep } from "~/utils/timer";
 
 
 export const processSetName = async (SetNameDom: React.Component): Promise<void> => {
-  const addressName = randomString(16);
+  const addressName = randomString(6);
+  console.log(`processSetName name to register -> ${addressName}`)
   const inputs = TestUtils.scryRenderedDOMComponentsWithTag(SetNameDom, "input");
   expect(inputs.length).toBe(1);
 
   const accountNameField = inputs[0];
   TestUtils.Simulate.change(accountNameField, { target: { value: addressName } } as any);
+  await sleep(2000)
 
   const form = TestUtils.findRenderedDOMComponentWithTag(SetNameDom, "form");
   if (!form) {
