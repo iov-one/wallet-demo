7cac327b125e8f4f8e9f560bb80906b1c3ced5f0
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-01-25 13:49:00 +0100

    Adjust SelectItems

--- src/components/forms/SelectField/SelectItems.tsx
+++ src/components/forms/SelectField/SelectItems.tsx
@@ -3,8 +3,7 @@ import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import * as React from "react";
 import Block from "~/components/layout/Block";
-import Grid from "~/components/layout/Grid";
-import GridItem from "~/components/layout/GridItem";
+import Hairline from "~/components/layout/Hairline";
 import Typography from "~/components/layout/Typography";
 import { Item } from "./index";
 
@@ -19,30 +18,25 @@ interface ListItemProps {
   readonly items: ReadonlyArray<Item>;
   readonly align: "left" | "right";
 }
+
 const ListItems = ({ phone, action, items, align }: ListItemProps) => {
   return (
-    <List disablePadding component="nav" style={style}>
+    <List component="nav" style={style}>
       {items.map(item => (
-        <ListItem key={item.name} disableGutters button onClick={action(item)}>
-          <ListItemText disableTypography>
-            <Block padding={!phone ? "sm" : undefined}>
-              <Grid>
-                <GridItem xs={12} sm={3}>
-                  <Typography align={align} inline variant={phone ? "body1" : "body2"}>
-                    {item.name}
-                  </Typography>
-                </GridItem>
-                {item.additionalText && (
-                  <GridItem xs={12} sm={9}>
-                    <Typography align={align} inline variant="body2">
-                      {item.additionalText}
-                    </Typography>
-                  </GridItem>
-                )}
-              </Grid>
-            </Block>
-          </ListItemText>
-        </ListItem>
+        <Block>
+          <ListItem key={item.name} button onClick={action(item)}>
+            <ListItemText
+              disableTypography
+              primary={
+                <Typography align={align} variant={phone ? "body1" : "body2"}>
+                  {item.name}
+                </Typography>
+              }
+              secondary={<Typography color="textSecondary">{item.additionalText}</Typography>}
+            />
+          </ListItem>
+          <Hairline />
+        </Block>
       ))}
     </List>
   );
