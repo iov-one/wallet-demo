d21466175d28b53d7944bace70c09fe10596ff72
Albert Andrejev <albert.andrejev@gmail.com>
2019-01-25 10:41:04 +0200

    Component update, remove unused variables

--- src/components/errors/ErrorBoundary.tsx
+++ src/components/errors/ErrorBoundary.tsx
@@ -1,27 +1,21 @@
-import { ClickAwayListener } from "@material-ui/core";
 import * as Sentry from "@sentry/browser";
 import * as React from "react";
-//import { SentryWidget } from "./SentryWidget";
 
 interface Props {
   readonly type: "inner" | "outer";
 }
 
 interface State {
-  //readonly error: Error | null;
   readonly openSentry: boolean;
 }
 
-const style = {};
 
 export default class ErrorBoundary extends React.Component<Props, State> {
   public readonly state = {
-    //error: null,
     openSentry: false,
   };
 
   public readonly componentDidCatch = (error: Error | null, errorInfo: any): void => {
-    //this.setState({ error });
     this.setState({ openSentry: true });
     Sentry.withScope(scope => {
       Object.keys(errorInfo).forEach(key => {
@@ -31,17 +25,13 @@ export default class ErrorBoundary extends React.Component<Props, State> {
     });
   };
 
-  public readonly closeSentry = () => {
-    this.setState({ openSentry: false });
-  }
-
   public render(): React.ReactNode {
     return this.state.openSentry ? (
-      <ClickAwayListener
-        onClickAway={this.closeSentry}
-      >
+      <React.Fragment>
         {Sentry.showReportDialog() as React.ReactNode}
-      </ClickAwayListener >
-    ) : this.props.children;
+      </React.Fragment>
+    ) : (
+      this.props.children
+    );
   }
 }
