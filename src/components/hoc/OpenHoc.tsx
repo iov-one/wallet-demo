197e988dfd6a8603614704ec0cb6f835b17d10b3
Albert Andrejev <albert.andrejev@gmail.com>
2019-01-28 16:29:32 +0200

    OpenHoc toggle callback fix

--- src/components/hoc/OpenHoc.tsx
+++ src/components/hoc/OpenHoc.tsx
@@ -19,7 +19,7 @@ export function openHoc<T>(
     { open: false, visited: false },
     {
       toggle: (state: OpenType) => (callBack?: () => void) => {
-        if (callBack) {
+        if (callBack && typeof callBack === "function") {
           callBack();
         }
         return { open: !state.open, visited: true };
