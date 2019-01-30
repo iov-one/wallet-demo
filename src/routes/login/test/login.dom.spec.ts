226d64e89fb0949404ad113c2cc798bddaaa552f
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-01-30 08:54:02 +0100

    Adding Login test simulating wrong password

--- src/routes/login/test/login.dom.spec.ts
+++ src/routes/login/test/login.dom.spec.ts
@@ -1,9 +1,12 @@
+import { UserProfile } from "@iov/keycontrol";
 import { Store } from "redux";
+// import * as profile from "~/logic/profile";
 import { mayTestBns } from "~/logic/testhelpers";
 import { RootState } from "~/reducers";
 import { BALANCE_ROUTE, LOGIN_ROUTE, SET_NAME_ROUTE } from "~/routes";
 import { processBalance, travelToBalance } from "~/routes/balance/test/util/travelBalance";
 import { TEST_PASS_PHRASE } from "~/routes/signupPass/test/utils/travelSignup";
+import { getProfileDB } from "~/selectors";
 import { shutdownSequence } from "~/sequences";
 import { aNewStore, resetHistory } from "~/store";
 import { expectRoute } from "~/utils/test/dom";
@@ -45,6 +48,28 @@ describe("DOM > Feature > Login", () => {
 
       await processLogin(loginDom, TEST_PASS_PHRASE);
       expectRoute(refreshStore, BALANCE_ROUTE);
+    },
+    30000,
+  );
+
+  mayTestBns(
+    `should fail if user introduces incorrect password`,
+    async () => {
+      // GIVEN
+      const loginDom = await travelToBalance(refreshStore);
+      const db = getProfileDB(refreshStore.getState());
+      const wrongPassword = "wrong password";
+      const loadProfileSpy = jest.spyOn(UserProfile, "loadFrom");
+
+      // WHEN
+      expectRoute(refreshStore, LOGIN_ROUTE);
+      await processLogin(loginDom, wrongPassword);
+
+      // THEN
+      expect(loadProfileSpy).toHaveBeenCalledTimes(1);
+      expect(loadProfileSpy).toHaveBeenLastCalledWith(db, wrongPassword);
+      await expect(UserProfile.loadFrom(db, wrongPassword)).rejects.toThrow("invalid usage");
+      expectRoute(refreshStore, LOGIN_ROUTE);
     },
     30000,
   );
