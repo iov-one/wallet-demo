2ff98e021440eea47c47e2aa5e645709870133bf
Albert Andrejev <albert.andrejev@gmail.com>
2019-02-01 12:43:56 +0200

    Remove comment

--- src/components/Header/store/actions/logoutProfile.ts
+++ src/components/Header/store/actions/logoutProfile.ts
@@ -1,6 +1,5 @@
 import { logoutProfileSyncAction } from "~/reducers/profile";
 
-// COMMENT: note you can also use typeof to do so.... but that would require naming the function before export default
 export type LogoutProfileType = () => Promise<void>;
 
 export default (dispatch: any) => dispatch(logoutProfileSyncAction());
