376f786dee8837c35c4e27c112782c19188317d9
Adolfo Panizo <adolfo.panizo@gmail.com>
2019-02-01 20:48:56 +0100

    Simplification of set name route

--- src/routes/signupName/container/index.tsx
+++ src/routes/signupName/container/index.tsx
@@ -1,11 +1,9 @@
-// tslint:disable:no-string-literal
 import * as React from "react";
 import { connect } from "react-redux";
 import { FormType } from "~/components/forms/Form";
 import { toastHoc, ToastType } from "~/components/hoc/ToastHoc";
 import { ToastVariant } from "~/context/ToastProvider";
-import { hasStoredProfile } from "~/logic";
-import { BALANCE_ROUTE, LOGIN_ROUTE, SIGNUP_ROUTE } from "~/routes";
+import { BALANCE_ROUTE } from "~/routes";
 import CreateUsername from "~/routes/signupName/components";
 import { USERNAME_FIELD } from "~/routes/signupName/components/FormComponent";
 import { history } from "~/store";
@@ -15,31 +13,6 @@ import selector, { SelectorProps } from "./selector";
 interface Props extends SignupNameActions, SelectorProps, ToastType {}
 
 class SignupName extends React.Component<Props> {
-  public async componentDidMount(): Promise<void> {
-    const { hasIdentity, chainId, accountName, db } = this.props;
-
-    if (!chainId) {
-      history.push(SIGNUP_ROUTE);
-
-      return;
-    }
-
-    const hasIdentityWithName = hasIdentity && accountName;
-    if (hasIdentityWithName) {
-      history.push(BALANCE_ROUTE);
-
-      return;
-    }
-
-    const hasProfile = await hasStoredProfile(db);
-    const hasProfileWithName = hasProfile && accountName;
-    if (hasProfileWithName) {
-      history.push(LOGIN_ROUTE);
-
-      return;
-    }
-  }
-
   public readonly onCreateUsername = async (values: object) => {
     const { setName, showToast } = this.props;
     const name = (values as FormType)[USERNAME_FIELD];
--- src/routes/signupName/container/selector.ts
+++ src/routes/signupName/container/selector.ts
@@ -1,31 +1,16 @@
 import { BnsConnection } from "@iov/bns";
-import { ChainId } from "@iov/core";
-import { createSelector, createStructuredSelector, Selector } from "reselect";
-import { StringDB } from "~/logic";
+import { createStructuredSelector, Selector } from "reselect";
 import { RootState } from "~/reducers";
-import { accountNameSelector, dbSelector, identitySelector } from "~/routes/home/container/selector";
-import { getChainIds, requireBnsConnection } from "~/selectors";
+import { accountNameSelector } from "~/routes/home/container/selector";
+import { requireBnsConnection } from "~/selectors";
 
 export interface SelectorProps {
-  readonly chainId: ChainId;
   readonly connection: BnsConnection;
-  readonly db: StringDB;
-  readonly hasIdentity: boolean;
   readonly accountName: string | undefined;
 }
 
-const chainIdSelector = createSelector(
-  getChainIds,
-  (chainIds: ReadonlyArray<ChainId>) => {
-    return chainIds[0];
-  },
-);
-
 const structuredSelector: Selector<RootState, SelectorProps> = createStructuredSelector({
-  chainId: chainIdSelector,
   connection: requireBnsConnection,
-  db: dbSelector,
-  hasIdentity: identitySelector,
   accountName: accountNameSelector,
 });
 
