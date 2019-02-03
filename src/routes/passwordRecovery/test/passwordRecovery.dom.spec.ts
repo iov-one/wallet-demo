3a95115d39047593afdaf9ab2b74bb775af3b900
Albert Andrejev <albert.andrejev@gmail.com>
2019-02-03 23:24:36 +0200

    Password recovery second step test

--- src/routes/passwordRecovery/test/passwordRecovery.dom.spec.ts
+++ src/routes/passwordRecovery/test/passwordRecovery.dom.spec.ts
@@ -1,13 +1,14 @@
 import { Ed25519HdWallet } from "@iov/keycontrol";
 import TestUtils from "react-dom/test-utils";
 import { Store } from "redux";
-//import { mayTestBns } from "~/logic/testhelpers";
 import { Toast } from "~/context/ToastProvider/Toast";
+import { mayTestBns, randomString } from "~/logic/testhelpers";
 import { RootState } from "~/reducers";
+import { SET_NAME_ROUTE } from "~/routes";
 import { shutdownSequence } from "~/sequences";
 import { aNewStore, resetHistory } from "~/store";
-import { PasswordRecoveryInternal } from "../container/index";
-import { getRandomMnemonic, getRealMnemonic, processPassRecovery, travelToPassRecovery } from "./util/travelPassRecovery";
+import { expectRoute } from "~/utils/test/dom";
+import { getRandomMnemonic, getRealMnemonic, PasswordFormData, processProfileRecovery, processUpdatePass, travelToProfileRecovery, travelToUpdatePass } from "./util/travelPassRecovery";
 
 describe("DOM > Feature > Password Recovery", () => {
   let store: Store<RootState>;
@@ -30,73 +31,135 @@ describe("DOM > Feature > Password Recovery", () => {
     shutdownSequence(null, store.getState);
   });
 
-  it(
-    "should have 12 input fields",
-    async () => {
-      const PassRecoveryDom = await travelToPassRecovery(refreshStore);
-      const inputs = TestUtils.scryRenderedDOMComponentsWithTag(PassRecoveryDom, "input");
-      expect(inputs.length).toBe(12);
-    },
-    16000,
-  );
+  describe("First step", () => {
+    it(
+      "should have 12 input fields",
+      async () => {
+        const RecoverProfileDom = await travelToProfileRecovery(refreshStore);
+        const inputs = TestUtils.scryRenderedDOMComponentsWithTag(RecoverProfileDom, "input");
+        expect(inputs.length).toBe(12);
+      },
+      16000,
+    );
 
-  it(
-    `should create wallet from mnemonic`,
-    async () => {
-      const PassRecoveryDom = await travelToPassRecovery(refreshStore);
-      const hdWalletSpy = jest.spyOn(Ed25519HdWallet, "fromMnemonic");
+    it(
+      `should create wallet from mnemonic`,
+      async () => {
+        const RecoverProfileDom = await travelToProfileRecovery(refreshStore);
+        const hdWalletSpy = jest.spyOn(Ed25519HdWallet, "fromMnemonic");
 
-      const mnemonic = getRandomMnemonic();
-      await processPassRecovery(PassRecoveryDom, mnemonic);
+        const mnemonic = getRandomMnemonic();
+        await processProfileRecovery(RecoverProfileDom, mnemonic);
 
-      expect(hdWalletSpy).toHaveBeenCalledTimes(1);
-      expect(hdWalletSpy).toHaveBeenLastCalledWith(mnemonic.join(" "));
-    },
-    16000,
-  );
+        expect(hdWalletSpy).toHaveBeenCalledTimes(1);
+        expect(hdWalletSpy).toHaveBeenLastCalledWith(mnemonic.join(" "));
+      },
+      16000,
+    );
 
-  it(
-    `should show toast in case of wrong mnemonics`,
-    async () => {
-      const PassRecoveryDom = await travelToPassRecovery(refreshStore);
+    it(
+      `should show toast in case of wrong mnemonics`,
+      async () => {
+        const RecoverProfileDom = await travelToProfileRecovery(refreshStore);
 
-      const mnemonic = getRandomMnemonic();
-      await processPassRecovery(PassRecoveryDom, mnemonic);
+        const mnemonic = getRandomMnemonic();
+        await processProfileRecovery(RecoverProfileDom, mnemonic);
 
-      const toastComponent = TestUtils.findRenderedComponentWithType(PassRecoveryDom, Toast);
-      //console.log(toastComponent.props);
-      expect(toastComponent.props.open).toBe(true);
-      expect(toastComponent.props.message).toBe("The recovery phrase you entered is invalid");
-      expect(toastComponent.props.variant).toBe("error");
-    },
-    16000,
-  );
+        const toastComponent = TestUtils.findRenderedComponentWithType(RecoverProfileDom, Toast);
+        expect(toastComponent.props.open).toBeTruthy();
+        expect(toastComponent.props.message).toBe("The recovery phrase you entered is invalid");
+        expect(toastComponent.props.variant).toBe("error");
+      },
+      16000,
+    );
 
-  it(
-    `should not show toast in case if mnemonics are right`,
-    async () => {
-      const PassRecoveryDom = await travelToPassRecovery(refreshStore);
+    it(
+      `should not show toast in case if mnemonics are right`,
+      async () => {
+        const RecoverProfileDom = await travelToProfileRecovery(refreshStore);
 
-      const mnemonic = getRealMnemonic();
-      await processPassRecovery(PassRecoveryDom, mnemonic);
+        const mnemonic = getRealMnemonic();
+        await processProfileRecovery(RecoverProfileDom, mnemonic);
 
-      const toastComponent = TestUtils.findRenderedComponentWithType(PassRecoveryDom, Toast);
-      expect(toastComponent.props.open).toBe(false);
-    },
-    16000,
-  );
+        const toastComponent = TestUtils.findRenderedComponentWithType(RecoverProfileDom, Toast);
+        expect(toastComponent.props.open).toBeFalsy();
+      },
+      16000,
+    );
+  });
 
-  it(
-    `should redirect to second step of password recovery`,
-    async () => {
-      const PassRecoveryDom = await travelToPassRecovery(refreshStore);
+  describe("Second step", () => {
+    it(
+      "should have 2 input fields",
+      async () => {
+        const UpdatePassDom = await travelToUpdatePass(refreshStore);
+        const inputs = TestUtils.scryRenderedDOMComponentsWithTag(UpdatePassDom, "input");
+        expect(inputs.length).toBe(2);
+      },
+      16000,
+    );
 
-      const mnemonic = getRealMnemonic();
-      await processPassRecovery(PassRecoveryDom, mnemonic);
+    it(
+      "submit should be disabled if input fields are empty",
+      async () => {
+        const UpdatePassDom = await travelToUpdatePass(refreshStore);
 
-      const recoveryComponent = TestUtils.findRenderedComponentWithType(PassRecoveryDom, PasswordRecoveryInternal);
-      expect(recoveryComponent.state.step).toBe("recover_password");
-    },
-    16000,
-  );
+        const button = TestUtils.findRenderedDOMComponentWithTag(UpdatePassDom, "button") as HTMLButtonElement;
+        expect(button.disabled).toBeTruthy();
+      },
+      16000,
+    );
+
+    it(
+      "submit should be disabled if input fields values are not equal",
+      async () => {
+        const passwords: PasswordFormData = {
+          password: randomString(10),
+          passwordConfirm: randomString(10),
+        };
+
+        const UpdatePassDom = await travelToUpdatePass(refreshStore);
+        await processUpdatePass(UpdatePassDom, passwords);        
+
+        const button = TestUtils.findRenderedDOMComponentWithTag(UpdatePassDom, "button") as HTMLButtonElement;
+        expect(button.disabled).toBeTruthy();
+      },
+      16000,
+    );
+
+    it(
+      "submit should be enabled if input fields values are equal",
+      async () => {
+        const password = randomString(10);
+        const passwords: PasswordFormData = {
+          password: password,
+          passwordConfirm: password,
+        };
+
+        const UpdatePassDom = await travelToUpdatePass(refreshStore);
+        await processUpdatePass(UpdatePassDom, passwords, false);        
+
+        const button = TestUtils.findRenderedDOMComponentWithTag(UpdatePassDom, "button") as HTMLButtonElement;
+        expect(button.disabled).toBeFalsy();
+      },
+      16000,
+    );
+
+    mayTestBns(
+      `should submit password and redirect to ${SET_NAME_ROUTE}`,
+      async () => {
+        const password = randomString(10);
+        const passwords: PasswordFormData = {
+          password: password,
+          passwordConfirm: password,
+        };
+
+        const UpdatePassDom = await travelToUpdatePass(refreshStore);
+        await processUpdatePass(UpdatePassDom, passwords);  
+
+        expectRoute(refreshStore, SET_NAME_ROUTE);
+      },
+      16000,
+    );
+  });
 });
--- src/routes/passwordRecovery/test/util/travelPassRecovery.ts
+++ src/routes/passwordRecovery/test/util/travelPassRecovery.ts
@@ -5,8 +5,14 @@ import { PASSWORD_RECOVERY_ROUTE } from "~/routes";
 import { history } from "~/store";
 import { createDom } from "~/utils/test/dom";
 import { sleep } from "~/utils/timer";
+import { PasswordRecoveryInternal } from "../../container";
 
-export const processPassRecovery = async (RecoveryDom: React.Component, mnemonic: ReadonlyArray<string>): Promise<void> => {
+export interface PasswordFormData {
+  readonly password: string;
+  readonly passwordConfirm: string;
+}
+
+export const processProfileRecovery = async (RecoveryDom: React.Component, mnemonic: ReadonlyArray<string>): Promise<void> => {
   const inputs = TestUtils.scryRenderedDOMComponentsWithTag(RecoveryDom, "input");
 
   for (let i = 0; i < mnemonic.length; i++) {
@@ -14,15 +20,12 @@ export const processPassRecovery = async (RecoveryDom: React.Component, mnemonic
   }
 
   const form = TestUtils.findRenderedDOMComponentWithTag(RecoveryDom, "form");
-  if (!form) {
-    throw new Error();
-  }
   TestUtils.Simulate.submit(form);
 
   await sleep(3000);
 }
 
-export const travelToPassRecovery = async (store: Store): Promise<React.Component> => {
+export const travelToProfileRecovery = async (store: Store): Promise<React.Component> => {
   history.push(PASSWORD_RECOVERY_ROUTE);
 
   const dom = createDom(store);
@@ -30,6 +33,35 @@ export const travelToPassRecovery = async (store: Store): Promise<React.Componen
 
   return dom;
 };
+
+export const travelToUpdatePass = async (store: Store): Promise<React.Component> => {
+  const dom = await travelToProfileRecovery(store);
+
+  const mnemonic = getRealMnemonic();
+  await processProfileRecovery(dom, mnemonic);
+
+  const recoveryComponent = TestUtils.findRenderedComponentWithType(dom, PasswordRecoveryInternal);
+  expect(recoveryComponent.state.step).toBe("recover_password");
+
+  return dom;
+};
+
+export const processUpdatePass = async (RecoveryDom: React.Component, passwords: PasswordFormData, submitForm: boolean = true): Promise<void> => {
+  const inputs = TestUtils.scryRenderedDOMComponentsWithTag(RecoveryDom, "input");
+
+  const password = inputs[0];
+  TestUtils.Simulate.change(password, { target: { value: passwords.password } } as any);
+
+  const passwordConfirm = inputs[1];
+  TestUtils.Simulate.change(passwordConfirm, { target: { value: passwords.passwordConfirm } } as any);
+
+  if (submitForm) {
+    const form = TestUtils.findRenderedDOMComponentWithTag(RecoveryDom, "form");
+    TestUtils.Simulate.submit(form);
+  }
+
+  await sleep(5000);
+}
 
 export const getRandomMnemonic = (): ReadonlyArray<string> => {
   // tslint:disable-next-line:readonly-array
