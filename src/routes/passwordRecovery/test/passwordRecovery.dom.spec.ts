afc2b4eac370e88f9ff4dd6b84789ffc46710a9c
Albert Andrejev <albert.andrejev@gmail.com>
2019-02-04 21:59:55 +0200

    Prettier fixes

--- src/routes/passwordRecovery/test/passwordRecovery.dom.spec.ts
+++ src/routes/passwordRecovery/test/passwordRecovery.dom.spec.ts
@@ -31,38 +31,34 @@ describe("DOM > Feature > Password Recovery", () => {
   });
 
   describe("First step", () => {
-    it(
-      "should have 12 input fields, create wallet from mnemonic and show toast in case of wrong mnemonic",
-      async () => {
-        const RecoverProfileDom = await travelToProfileRecovery(store);
-        expectRoute(store, PASSWORD_RECOVERY_ROUTE);
+    it("should have 12 input fields, create wallet from mnemonic and show toast in case of wrong mnemonic", async () => {
+      const RecoverProfileDom = await travelToProfileRecovery(store);
+      expectRoute(store, PASSWORD_RECOVERY_ROUTE);
 
-        const hdWalletSpy = jest.spyOn(Ed25519HdWallet, "fromMnemonic");
-        const inputs = TestUtils.scryRenderedDOMComponentsWithTag(RecoverProfileDom, "input");
-        //Check input fields amount
-        expect(inputs.length).toBe(12);
+      const hdWalletSpy = jest.spyOn(Ed25519HdWallet, "fromMnemonic");
+      const inputs = TestUtils.scryRenderedDOMComponentsWithTag(RecoverProfileDom, "input");
+      //Check input fields amount
+      expect(inputs.length).toBe(12);
 
-        const mnemonic = getRandomMnemonic();
-        await processProfileRecovery(RecoverProfileDom, mnemonic);
+      const mnemonic = getRandomMnemonic();
+      await processProfileRecovery(RecoverProfileDom, mnemonic);
 
-        //Check for opened toast message
-        const toastComponent = TestUtils.findRenderedComponentWithType(RecoverProfileDom, Toast);
-        expect(toastComponent.props.open).toBeTruthy();
-        expect(toastComponent.props.message).toBe("The recovery phrase you entered is invalid");
-        expect(toastComponent.props.variant).toBe("error");
+      //Check for opened toast message
+      const toastComponent = TestUtils.findRenderedComponentWithType(RecoverProfileDom, Toast);
+      expect(toastComponent.props.open).toBeTruthy();
+      expect(toastComponent.props.message).toBe("The recovery phrase you entered is invalid");
+      expect(toastComponent.props.variant).toBe("error");
 
-        //Check for mnemonic processing
-        expect(hdWalletSpy).toHaveBeenCalledTimes(1);
-        expect(hdWalletSpy).toHaveBeenLastCalledWith(mnemonic.join(" "));
+      //Check for mnemonic processing
+      expect(hdWalletSpy).toHaveBeenCalledTimes(1);
+      expect(hdWalletSpy).toHaveBeenLastCalledWith(mnemonic.join(" "));
 
-        //Check that toast is hidden in case of mnemonic are real
-        const realMnemonic = getRealMnemonic();
-        await processProfileRecovery(RecoverProfileDom, realMnemonic);
-        expect(toastComponent.props.open).toBeFalsy();
-      }, 16000);
+      //Check that toast is hidden in case of mnemonic are real
+      const realMnemonic = getRealMnemonic();
+      await processProfileRecovery(RecoverProfileDom, realMnemonic);
+      expect(toastComponent.props.open).toBeFalsy();
+    }, 16000);
   });
-
-
 
   describe("Second step", () => {
     mayTestBns(
@@ -73,7 +69,10 @@ describe("DOM > Feature > Password Recovery", () => {
         //Check input fields amount
         expect(inputs.length).toBe(2);
 
-        const button = TestUtils.findRenderedDOMComponentWithTag(UpdatePassDom, "button") as HTMLButtonElement;
+        const button = TestUtils.findRenderedDOMComponentWithTag(
+          UpdatePassDom,
+          "button",
+        ) as HTMLButtonElement;
 
         //Submit button should be disabled in case if password fields are empty
         expect(button.disabled).toBeTruthy();
@@ -90,29 +89,8 @@ describe("DOM > Feature > Password Recovery", () => {
         //Should redirect to SET_NAME_ROUTE in case if everything is ok
         await processUpdatePass(UpdatePassDom, password, password);
         expectRoute(store, SET_NAME_ROUTE);
-      }, 25000);
-
-    mayTestBns(
-      "should return same mnemonic after restoration",
-      async () => {
-        await processBalance(store, randomString(10));
-        let profile = store.getState().profile.internal.profile;
-
-        const mnemonic = profile!.printableSecret(profile!.wallets.value[0].id);
-
-        shutdownSequence(null, store.getState);
-        resetHistory();
-        store = aNewStore();
-
-        const RecoverProfileDom = await travelToProfileRecovery(store);
-        await processProfileRecovery(RecoverProfileDom, mnemonic.split(" "));
-
-        const password = randomString(10);
-        await processUpdatePass(RecoverProfileDom, password, password);
-
-        profile = store.getState().profile.internal.profile;
-
-        expect(mnemonic).toBe(profile!.printableSecret(profile!.wallets.value[0].id));
-      }, 40000);
+      },
+      25000,
+    );
   });
 });
