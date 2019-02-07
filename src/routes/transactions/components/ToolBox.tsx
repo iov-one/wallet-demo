f5e99453082d7388ddf6f88f62df6c93092875b5
Albert Andrejev <albert.andrejev@gmail.com>
2019-02-07 15:32:33 +0200

    TeoolBox: Desktop

--- src/routes/transactions/components/ToolBox.tsx
+++ src/routes/transactions/components/ToolBox.tsx
@@ -1,41 +1,59 @@
 import { createStyles, Fab, withStyles, WithStyles } from "@material-ui/core";
 import * as React from "react";
+import Block from "~/components/layout/Block";
 import CircleImage from "~/components/layout/CircleImage";
 import Typography from "~/components/layout/Typography";
-import { background, primary, xs } from "~/theme/variables";
+import { background, border, primary, xl, xs } from "~/theme/variables";
 import download from "../assets/download.svg";
 
 const styles = createStyles({
   root: {
-    width: "auto",    
+    width: 150,
     justifyContent: "left",
     textTransform: "inherit",
   },
   sizeSmall: {
-    height: 32,
+    height: xl,
   },
   text: {
     paddingLeft: xs,
   },
   secondary: {
-    backgroundColor: background
+    backgroundColor: background,
+    padding: 0,
+    boxShadow: "none",
+    border: `1px solid ${border}`,
+    "&:hover": {
+      background: background
+    }
+  },
+  panel: {
+    height: 64,
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: background,
   }
 });
 
-interface Props extends WithStyles<typeof styles> {}
+interface Props extends WithStyles<typeof styles> {
+  readonly phone: boolean;
+}
 
-const ToolBox = ({classes}: Props): JSX.Element => {
+const ToolBox = ({ classes, phone }: Props): JSX.Element => {
   return (
-    <Fab
-      variant="extended"
-      size="small"
-      color="secondary"
-      aria-label="Export as CSV"
-      classes={classes}
-    >
-      <CircleImage icon={download} circleColor={primary} alt="Download" dia={32} />
-      <Typography variant="subtitle2" weight="regular" className={classes.text}>Export as .CSV</Typography>
-    </Fab>
+    <Block className={classes.panel} padding="lg">
+      {!phone && <Block maxWidth={176} grow />}
+      <Fab
+        variant="extended"
+        size="small"
+        color="secondary"
+        aria-label="Export as CSV"
+        classes={classes}
+      >
+        <CircleImage icon={download} circleColor={primary} alt="Download" dia={32} width={16} height={16} />
+        <Typography variant="subtitle2" weight="regular" className={classes.text}>Export as .CSV</Typography>
+      </Fab>
+    </Block>
   );
 };
 
