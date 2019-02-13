0b7277da6dacf295ae583d3bc059f38839c67224
Albert Andrejev <albert.andrejev@gmail.com>
2019-02-14 00:09:29 +0200

    Show transactions (brief)

--- src/routes/transactions/components/TransactionsTable.tsx
+++ src/routes/transactions/components/TransactionsTable.tsx
@@ -1,4 +1,4 @@
-import { createStyles, withStyles, WithStyles } from "@material-ui/core";
+import { createStyles, withStyles, WithStyles, Select, MenuItem } from "@material-ui/core";
 import * as React from "react";
 import Block from "~/components/layout/Block";
 import Hairline from "~/components/layout/Hairline";
@@ -7,6 +7,7 @@ import Spacer from "~/components/layout/Spacer";
 import Typography from "~/components/layout/Typography";
 import { background, md } from "~/theme/variables";
 import sorting from "../assets/sorting.svg";
+import TransactionRow from "./TransactionRow";
 
 const styles = createStyles({
   panel: {
@@ -17,24 +18,35 @@ const styles = createStyles({
   column: {
     display: "flex",
     flexDirection: "column",
-    margin: `${md} 0`,
   },
   header: {
     display: "flex",
     alignItems: "center",
+    margin: `${md} 0`,
   },
-  row: {},
+  footer: {
+    display: "flex",
+    alignItems: "center",
+    margin: `${md} 0`,
+  },
+  row: {
+    display: "flex",
+    margin: `${md} 0`,
+  },
+  dropdownArrow: {
+    marginTop: 10,
+  }
 });
 
-interface Props extends WithStyles<typeof styles> {}
+interface Props extends WithStyles<typeof styles> { }
 
 const TransactionsTable = ({ classes }: Props): JSX.Element => {
   return (
     <React.Fragment>
       <Block margin="lg" />
       <Block className={classes.panel}>
-        <Block padding="lg" className={classes.column}>
-          <Block className={classes.header}>
+        <Block className={classes.column}>
+          <Block padding="lg" className={classes.header}>
             <Typography variant="subtitle2" weight="semibold">
               Transactions
             </Typography>
@@ -42,10 +54,27 @@ const TransactionsTable = ({ classes }: Props): JSX.Element => {
             <Img src={sorting} width={24} height={24} alt="Sorting" />
           </Block>
           <Hairline />
-          <Block className={classes.row}>
-            <Typography variant="subtitle2" weight="semibold">
-              Transactions
+          <TransactionRow type="send" address="alex*iov" amount="-0.01762507" symbol="BTC" time="1:00 am &#183; 20 Nov 2018" />
+          <TransactionRow type="receive" address="cathy*iov" amount="+507" symbol="IOV" time="1:00 am &#183; 20 Nov 2018" />
+          <TransactionRow type="reject" address="alex*iov" amount="-0.01762507" symbol="BTC" time="1:00 am &#183; 20 Nov 2018" />
+          <TransactionRow type="receive" address="blockchain address" amount="+0.1757" symbol="ETH" time="1:00 am &#183; 20 Nov 2018" />
+          <Block padding="lg" className={classes.footer}>
+            <Typography variant="subtitle2" weight="regular">
+              Rows per page
             </Typography>
+            <Select
+              inputProps={{
+                name: 'rowsPerPage',
+                id: 'age-simple',
+              }}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value={5}>5</MenuItem>
+              <MenuItem value={10}>10</MenuItem>
+              <MenuItem value={20}>20</MenuItem>
+            </Select>
             <Spacer order={1} />
             <Img src={sorting} width={24} height={24} alt="Sorting" />
           </Block>
