db7f466bd78eea2e3a10ad7c0c5493d9f685504f
Albert Andrejev <albert.andrejev@gmail.com>
2019-02-14 14:17:43 +0200

    Transactions Table Header component

---
+++ src/routes/transactions/components/TransactionsTableHeader.tsx
@@ -0,0 +1,43 @@
+import { createStyles, WithStyles, withStyles } from "@material-ui/core";
+import * as React from "react";
+import Block from "~/components/layout/Block";
+import Hairline from "~/components/layout/Hairline";
+import Img from "~/components/layout/Image";
+import Spacer from "~/components/layout/Spacer";
+import Typography from "~/components/layout/Typography";
+import { background, md } from "~/theme/variables";
+import sorting from "../assets/sorting.svg";
+
+const styles = createStyles({
+  panel: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: background,
+  },
+  column: {
+    display: "flex",
+    flexDirection: "column",
+  },
+  header: {
+    display: "flex",
+    alignItems: "center",
+    margin: `${md} 0`,
+  },
+});
+
+interface Props extends WithStyles<typeof styles> {}
+
+const TransactionsTableHeader = ({ classes }: Props) => (
+  <React.Fragment>
+    <Block padding="lg" className={classes.header}>
+      <Typography variant="subtitle2" weight="semibold">
+        Transactions
+      </Typography>
+      <Spacer order={1} />
+      <Img src={sorting} width={24} height={24} alt="Sorting" />
+    </Block>
+    <Hairline />
+  </React.Fragment>
+);
+
+export default withStyles(styles)(TransactionsTableHeader);
