7b1eb76ec2e779e1206b49079a0554d645a6b17d
Albert Andrejev <albert.andrejev@gmail.com>
2019-02-14 14:18:17 +0200

    Transactions Table Row component

--- src/routes/transactions/components/TransactionRow.tsx
+++ src/routes/transactions/components/TransactionRow.tsx
@@ -9,12 +9,12 @@ import Typography from "~/components/layout/Typography";
 import { background, border, md } from "~/theme/variables";
 import dropdownArrow from "../assets/dropdownArrow.svg";
 import toAddress from "../assets/toAddress.svg";
-import toAddressRejected from "../assets/toAddressRejected.svg"
+import toAddressRejected from "../assets/toAddressRejected.svg";
 
 const styles = createStyles({
   row: {
     display: "flex",
-    
+
     flexDirection: "column",
   },
   rowContent: {
@@ -23,12 +23,12 @@ const styles = createStyles({
   },
   dropdownArrow: {
     marginTop: 10,
-  }
+  },
 });
 
 type txType = "send" | "receive" | "reject";
 
-interface Props extends WithStyles<typeof styles> { 
+interface Props extends WithStyles<typeof styles> {
   readonly type: txType;
   readonly address: string;
   readonly amount: string;
@@ -37,24 +37,38 @@ interface Props extends WithStyles<typeof styles> {
 }
 
 const getTypeIcon = (type: txType): string => {
-  switch(type) {
-    case "send" : return toAddress;
-    case "reject": return toAddressRejected;
-    case "receive": return toAddress; //TODO: change to proper icon as soon it will be available
+  switch (type) {
+    case "send":
+      return toAddress;
+    case "reject":
+      return toAddressRejected;
+    case "receive":
+      return toAddress; //TODO: change to proper icon as soon it will be available
   }
-}
+};
 
 const getAddressPrefix = (type: txType): string => {
-  switch(type) {
-    case "send" : return "To";
-    case "reject": return "From";
-    case "receive": return "To"
+  switch (type) {
+    case "send":
+      return "To";
+    case "reject":
+      return "To";
+    case "receive":
+      return "From";
   }
-}
+};
 const TransactionRow = ({ classes, type, address, amount, symbol, time }: Props): JSX.Element => (
   <Block padding="lg" className={classes.row}>
     <Block className={classes.rowContent}>
-      <CircleImage icon={getTypeIcon(type)} circleColor={background} borderColor={border} alt="Transaction type" dia={40} width={19} height={16} />
+      <CircleImage
+        icon={getTypeIcon(type)}
+        circleColor={background}
+        borderColor={border}
+        alt="Transaction type"
+        dia={40}
+        width={19}
+        height={16}
+      />
       <Block padding="md">
         <Typography variant="subtitle2" weight="semibold">
           {getAddressPrefix(type)} {address}
