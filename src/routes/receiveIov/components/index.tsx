c7f9270cef48803a082cbafbb91363cab90f7dd5
LucasIsasmendi <lucas.isasmendi@gmail.com>
2019-02-21 16:43:53 +0100

    Adjust ui receive iov payment

--- src/routes/receiveIov/components/index.tsx
+++ src/routes/receiveIov/components/index.tsx
@@ -1,13 +1,22 @@
-import { createStyles, Paper, Typography, withStyles, WithStyles } from "@material-ui/core";
+import { createStyles, withStyles, WithStyles } from "@material-ui/core";
 import * as React from "react";
-import { ConfirmInput, TooltipDescription } from "~/components/compoundComponents/form";
+import { ConfirmInput } from "~/components/compoundComponents/form";
 import Block from "~/components/layout/Block";
-import { primary, xl, xxl } from "~/theme/variables";
+import Tooltip from "~/components/layout/dialogs/Tooltip";
+import Typography from "~/components/layout/Typography";
+import { background, primary, xl, xxl } from "~/theme/variables";
 
 const styles = createStyles({
   container: {
     display: "flex",
     justifyContent: "center",
+  },
+  card: {
+    backgroundColor: background,
+    display: "flex",
+    flexDirection: "column",
+    flexBasis: "506px",
+    maxWidth: "506px",
   },
   wrapper: {
     flexBasis: "506px",
@@ -27,11 +36,16 @@ const styles = createStyles({
     paddingBottom: xxl,
   },
   mainText: {
+    gutterBottom: xl,
     marginBottom: xl,
-    component: "h2",
   },
   highlight: {
     color: primary,
+  },
+  tooltip: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
   },
 });
 
@@ -39,30 +53,56 @@ interface Props extends WithStyles<typeof styles> {
   readonly iovAddress: string;
 }
 
-class ReceiveIov extends React.Component<Props> {
+interface State {
+  readonly howItWorksHook: HTMLDivElement | null;
+}
+
+class ReceiveIov extends React.Component<Props, State> {
+  public readonly state = {
+    howItWorksHook: null,
+  };
+  private readonly howItWorksHookRef = React.createRef<HTMLDivElement>();
+
+  public componentDidMount(): void {
+    this.setState(() => ({
+      howItWorksHook: this.howItWorksHookRef.current,
+    }));
+  }
+
   public render(): JSX.Element {
     const { iovAddress, classes } = this.props;
     return (
-      <Block className={classes.container}>
-          <Paper className={classes.modalPaper}>
-            <Typography className={classes.mainText}>
+      <React.Fragment>
+        <Block padding="lg" margin="lg" />
+        <Block className={classes.container}>
+          <Block padding="lg" margin="lg" className={classes.card}>
+            <Block margin="xl" />
+            <Typography variant="title" weight="light">
               Receive payment from <b className={classes.highlight}>IOV wallet users</b> by giving them your
               IOV address
             </Typography>
+            <Block margin="xl" />
             <ConfirmInput
               title="Your IOV Address"
               value={iovAddress}
               notification="IOV Address copied to clipboard"
             />
-            <div className={classes.actionWrapper}>
-              <TooltipDescription
-                reversed
-                label="How it works"
-                info="Receive payments from anyone with an IOV wallet. Give them your IOV username and the funds will get send directly to your wallet"
-              />
-            </div>
-          </Paper>
-      </Block>
+            <Block margin="lg" />
+            <Block margin="sm" className={classes.tooltip}>
+              <Typography inline variant="body2">
+                How it works
+              </Typography>
+              <Block padding="xs" />
+              <Tooltip phoneHook={this.state.howItWorksHook}>
+                <Typography variant="body2">
+                  Receive payments from anyone with an IOV wallet. Give them your IOV username and the funds will get send directly to your wallet
+                </Typography>
+              </Tooltip>
+            </Block>
+            <Block margin="xl" />
+          </Block>
+        </Block>
+      </React.Fragment>
     );
   }
 }
