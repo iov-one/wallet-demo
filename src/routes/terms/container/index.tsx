fbbce93bfdb0c17e899231459080db5325d29985
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-05 19:24:58 +0200

    Update Terms components to use Text Markup

--- src/routes/terms/container/index.tsx
+++ src/routes/terms/container/index.tsx
@@ -2,10 +2,13 @@ import * as React from "react";
 import PageMenuColumn from "~/components/pages/PageMenuColumn";
 import Layout from "../components";
 
-const Terms = (): JSX.Element => (
-  <PageMenuColumn phoneFullWidth>
-    <Layout />
-  </PageMenuColumn>
-);
 
-export default Terms;
+export default class Terms extends React.Component {
+  public render(): JSX.Element {
+    return (
+      <PageMenuColumn phoneFullWidth>
+        <Layout />
+      </PageMenuColumn>
+    );
+  }
+}
