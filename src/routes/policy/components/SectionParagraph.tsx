af66311ddc5e64c505b71786fc5e6dfe6f50cd63
Albert Andrejev <albert.andrejev@gmail.com>
2019-03-05 11:17:52 +0200

    Paragraph component

---
+++ src/routes/policy/components/SectionParagraph.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import Typography from "~/components/layout/Typography";
+
+
+interface Props {
+  readonly children: React.ReactNode;
+}
+
+const SectionParagraph = ({ children }: Props) => (
+  <Typography weight="light" variant="body2" gutterBottom>
+    {children}
+  </Typography>
+);
+
+export default SectionParagraph;
